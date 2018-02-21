ac5416e119ca6702dcf2cb0befa44309a706b73f
Matthew Leibowitz <mattleibow@live.com>
2018-02-21 23:05:47 +0200

    Removed the demo member

--- F50/Preferences/Preferences.android.cs
+++ F50/Preferences/Preferences.android.cs
@@ -6,10 +6,6 @@ namespace Xamarin.F50
 {
 	public partial class Preferences
 	{
-		public Preferences(global::Android.Content.Context context)
-		{
-		}
-
 		public void Set(string key, string val)
 		{
 
