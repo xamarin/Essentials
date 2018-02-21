12e8f8ed0ac0baf032032fa813e14be7883be533
Matthew Leibowitz <mattleibow@live.com>
2018-02-21 07:35:06 +0200

    Added the bits to get mdoc working

--- F50/Preferences/Preferences.android.cs
+++ F50/Preferences/Preferences.android.cs
@@ -6,6 +6,10 @@ namespace Xamarin.F50
 {
 	public partial class Preferences
 	{
+		public Preferences(global::Android.Content.Context context)
+		{
+		}
+
 		public void Set(string key, string val)
 		{
 
