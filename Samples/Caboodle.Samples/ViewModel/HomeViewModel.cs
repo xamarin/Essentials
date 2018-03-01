3f899e7a113f90b149563d09c5683d8f5915845a
Matthew Leibowitz <mattleibow@live.com>
2018-03-01 15:20:05 +0200

    Merge branch 'master' into feature/phone-dialer

---
+++ Samples/Caboodle.Samples/ViewModel/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MvvmHelpers;
+using Caboodle.Samples.Model;
+using Caboodle.Samples.View;
+
+namespace Caboodle.Samples.ViewModel
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public HomeViewModel()
+        {
+            Items = new ObservableRangeCollection<SampleItem>
+            {
+                new SampleItem("Geocoding", typeof(GeocodingPage), "Easily geocode and reverse geocoding."),
+                new SampleItem("Preferences", typeof(PreferencesPage), "Quickly and easily add persistent preferences."),
+            };
+        }
+
+        public ObservableRangeCollection<SampleItem> Items { get; }
+    }
+}
