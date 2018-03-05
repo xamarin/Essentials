e9c34f509638d5669d1d6b0ce55e1ae792db3025
James Montemagno <james.montemagno@gmail.com>
2018-03-05 12:15:55 -0800

    Add back battery sample

--- Samples/Caboodle.Samples/ViewModel/BatteryViewModel.cs
+++ Samples/Caboodle.Samples/ViewModel/BatteryViewModel.cs
@@ -1,24 +1,64 @@
-﻿using Microsoft.Caboodle;
+﻿using System.Windows.Input;
+using Microsoft.Caboodle;
+using Xamarin.Forms;
 
 namespace Caboodle.Samples.ViewModel
 {
     public class BatteryViewModel : BaseViewModel
     {
+        public string Level => $"Charge Level: {Battery.ChargeLevel}";
+
+        public string State => $"State: {Battery.State}";
+
+        public string PowerSource => $"Power Source: {Battery.PowerSource}";
+
+        public string ListenText { get; set; } = "Listen for events";
+
+        public ICommand RefreshStatsCommand { get; }
+
+        public ICommand ListenCommand { get; }
+
+        bool listening;
+
         public BatteryViewModel()
         {
+            RefreshStatsCommand = new Command(() =>
+            {
+                OnPropertyChanged(nameof(Level));
+                OnPropertyChanged(nameof(State));
+                OnPropertyChanged(nameof(PowerSource));
+            });
+
+            ListenCommand = new Command(() =>
+            {
+                if (!listening)
+                {
+                    Battery.BatteryChanged += Battery_BatteryChanged;
+                    ListenText = "Stop listening";
+                }
+                else
+                {
+                    Battery.BatteryChanged -= Battery_BatteryChanged;
+                    ListenText = "Listen for events";
+                }
+
+                listening = !listening;
+                OnPropertyChanged(nameof(ListenText));
+            });
         }
 
-        public double Level => Battery.ChargeLevel;
-
-        public BatteryState State => Battery.State;
-
-        public BatteryPowerSource PowerSource => Battery.PowerSource;
-
-        public void Update(BatteryChangedEventArgs e)
+        async void Battery_BatteryChanged(BatteryChangedEventArgs e)
         {
-            OnPropertyChanged(nameof(Level));
-            OnPropertyChanged(nameof(State));
-            OnPropertyChanged(nameof(PowerSource));
+            RefreshStatsCommand.Execute(null);
+            await Application.Current.MainPage.DisplayAlert("Battery Event", $"Battery status changes\n Level: {e.ChargeLevel}\n Source: {e.PowerSource}\n State: {e.State}", "OK");
+        }
+
+        public void Stop()
+        {
+            if (listening)
+            {
+                Battery.BatteryChanged -= Battery_BatteryChanged;
+            }
         }
     }
 }
