2155fbe695e057f092ef350e1ba09f0fde4ddb90
Konstantin Chuvilyov <f1nzer@outlook.com>
2018-03-08 18:40:50 +0700

    added phone dialer sample

---
+++ Samples/Caboodle.Samples/ViewModel/PhoneDialerViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using Microsoft.Caboodle;
+using Xamarin.Forms;
+
+namespace Caboodle.Samples.ViewModel
+{
+    public class PhoneDialerViewModel : BaseViewModel
+    {
+        string phoneNumber;
+
+        public PhoneDialerViewModel()
+        {
+            OpenPhoneDialerCommand = new Command(OnOpenPhoneDialer);
+        }
+
+        public ICommand OpenPhoneDialerCommand { get; }
+
+        public bool IsEnabled => PhoneDialer.IsSupported && !string.IsNullOrEmpty(PhoneNumber);
+
+        public string IsSupportedMessage => $"Is supported? {PhoneDialer.IsSupported}";
+
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            set
+            {
+                if (SetProperty(ref phoneNumber, value))
+                {
+                    OnPropertyChanged(nameof(IsEnabled));
+                }
+            }
+        }
+
+        void OnOpenPhoneDialer() => PhoneDialer.Open(PhoneNumber);
+    }
+}
