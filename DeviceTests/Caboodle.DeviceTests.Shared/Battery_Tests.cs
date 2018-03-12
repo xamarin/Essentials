c3dcd52c1ce8f49bf29b0e52874fccfc115ea73f
Matthew Leibowitz <mattleibow@live.com>
2018-03-13 01:06:49 +0200

    Improve the battery test for better failure messages

--- DeviceTests/Caboodle.DeviceTests.Shared/Battery_Tests.cs
+++ DeviceTests/Caboodle.DeviceTests.Shared/Battery_Tests.cs
@@ -10,14 +10,14 @@ namespace Caboodle.DeviceTests
     {
         [Fact]
         public void Charge_Level() =>
-            Assert.True(Battery.ChargeLevel > 0.0);
+            Assert.InRange(Battery.ChargeLevel, 0.01, 100.0);
 
         [Fact]
         public void Charge_State() =>
-            Assert.True(Battery.State != BatteryState.Unknown);
+            Assert.NotEqual(BatteryState.Unknown, Battery.State);
 
         [Fact]
         public void Charge_Power() =>
-            Assert.True(Battery.PowerSource != BatteryPowerSource.Unknown);
+            Assert.NotEqual(BatteryPowerSource.Unknown, Battery.PowerSource);
     }
 }
