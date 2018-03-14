107e11e10e45548507ffb064fac5f209ba3b3a93
James Montemagno <james.montemagno@gmail.com>
2018-03-13 18:39:22 -0700

    Revert back sample, woops

--- Samples/Caboodle.Samples/ViewModel/BatteryViewModel.cs
+++ Samples/Caboodle.Samples/ViewModel/BatteryViewModel.cs
@@ -6,52 +6,15 @@ namespace Caboodle.Samples.ViewModel
 {
     public class BatteryViewModel : BaseViewModel
     {
-        public string Level => $"Charge Level: {Battery.ChargeLevel}";
-
-        public string State => $"State: {Battery.State}";
-
-        public string PowerSource => $"Power Source: {Battery.PowerSource}";
-
-        public string ListenText { get; set; } = "Listen for events";
-
-        public ICommand RefreshStatsCommand { get; }
-
-        public ICommand ListenCommand { get; }
-
-        bool listening;
-
         public BatteryViewModel()
         {
-            RefreshStatsCommand = new Command(() =>
-            {
-                OnPropertyChanged(nameof(Level));
-                OnPropertyChanged(nameof(State));
-                OnPropertyChanged(nameof(PowerSource));
-            });
-
-            ListenCommand = new Command(() =>
-            {
-                if (!listening)
-                {
-                    Battery.BatteryChanged += Battery_BatteryChanged;
-                    ListenText = "Stop listening";
-                }
-                else
-                {
-                    Battery.BatteryChanged -= Battery_BatteryChanged;
-                    ListenText = "Listen for events";
-                }
-
-                listening = !listening;
-                OnPropertyChanged(nameof(ListenText));
-            });
         }
 
-        async void Battery_BatteryChanged(BatteryChangedEventArgs e)
-        {
-            RefreshStatsCommand.Execute(null);
-            await Application.Current.MainPage.DisplayAlert("Battery Event", $"Battery status changes\n Level: {e.ChargeLevel}\n Source: {e.PowerSource}\n State: {e.State}", "OK");
-        }
+        public double Level => Battery.ChargeLevel;
+
+        public BatteryState State => Battery.State;
+
+        public BatteryPowerSource PowerSource => Battery.PowerSource;
 
         public override void OnAppearing()
         {
@@ -69,10 +32,9 @@ namespace Caboodle.Samples.ViewModel
 
         void OnBatteryChanged(BatteryChangedEventArgs e)
         {
-            if (listening)
-            {
-                Battery.BatteryChanged -= Battery_BatteryChanged;
-            }
+            OnPropertyChanged(nameof(Level));
+            OnPropertyChanged(nameof(State));
+            OnPropertyChanged(nameof(PowerSource));
         }
     }
 }
