b7a6f1e40881936afdb27b808e344800db282f1c
Matthew Leibowitz <mattleibow@live.com>
2018-03-15 20:36:56 +0200

    Using the new exceptions

--- Caboodle/FileSystem/FileSystem.netstandard.cs
+++ Caboodle/FileSystem/FileSystem.netstandard.cs
@@ -6,12 +6,12 @@ namespace Microsoft.Caboodle
     public static partial class FileSystem
     {
         public static string CacheDirectory
-            => throw new NotImplentedInReferenceAssembly();
+            => throw new NotImplementedInReferenceAssemblyException();
 
         public static string AppDataDirectory
-            => throw new NotImplentedInReferenceAssembly();
+            => throw new NotImplementedInReferenceAssemblyException();
 
         public static Task<Stream> OpenAppPackageFileAsync(string filename)
-             => throw new NotImplentedInReferenceAssembly();
+             => throw new NotImplementedInReferenceAssemblyException();
     }
 }
