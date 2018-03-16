f710d7703dbe0ce3599cb0ae96be8deb542628cc
Matthew Leibowitz <mattleibow@live.com>
2018-03-16 05:23:10 +0200

    Added tests

---
+++ Caboodle.Tests/Sms_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Caboodle.Tests
+{
+    public class Sms_Tests
+    {
+        [Fact]
+        public void Sms_Fail_On_NetStandard()
+        {
+            Assert.Throws<NotImplementedInReferenceAssemblyException>(() => Sms.IsComposeSupported);
+        }
+    }
+}
---
+++ DeviceTests/Caboodle.DeviceTests.Shared/Sms_Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Caboodle;
+using Xunit;
+
+namespace Caboodle.DeviceTests
+{
+    public class Sms_Tests
+    {
+        [Fact]
+        public Task Sms_Is_Supported()
+        {
+            return Utils.OnMainThread(() =>
+            {
+                if (Utils.IsiOSSimulator)
+                    Assert.False(Sms.IsComposeSupported);
+                else
+                    Assert.True(Sms.IsComposeSupported);
+            });
+        }
+
+        [Fact]
+        public Task Sms_ComposeAsync_Throws_When_Empty()
+        {
+            var message = new SmsMessage();
+
+            return Assert.ThrowsAsync<ArgumentException>(() => Sms.ComposeAsync(message));
+        }
+
+        [Fact]
+        public Task Sms_ComposeAsync_Throws_With_Null_Body()
+        {
+            var message = new SmsMessage
+            {
+                Recipient = "something"
+            };
+
+            return Assert.ThrowsAsync<ArgumentException>("Body", () => Sms.ComposeAsync(message));
+        }
+
+        [Fact]
+        public Task Sms_ComposeAsync_Throws_With_Null_Recipient()
+        {
+            var message = new SmsMessage
+            {
+                Body = "something"
+            };
+
+            return Assert.ThrowsAsync<ArgumentException>("Recipient", () => Sms.ComposeAsync(message));
+        }
+    }
+}
