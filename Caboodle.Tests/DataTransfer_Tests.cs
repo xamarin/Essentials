290f92ad0263aa175a03980a86ba22f9844a4072
James Montemagno <james.montemagno@gmail.com>
2018-03-18 21:35:59 -0700

    Add Tests for DataTransfer

---
+++ Caboodle.Tests/DataTransfer_Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Caboodle;
+using Xunit;
+
+namespace Caboodle.Tests
+{
+    public class DataTransfer_Tests
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task Show_Share_UI_Text_NetStandard() =>
+            await Assert.ThrowsAsync<NotImplementedInReferenceAssemblyException>(() => DataTransfer.ShowShareUI("Text"));
+
+        [Fact]
+        public async System.Threading.Tasks.Task Show_Share_UI_Text_Title_NetStandard() =>
+            await Assert.ThrowsAsync<NotImplementedInReferenceAssemblyException>(() => DataTransfer.ShowShareUI("Text", "Title"));
+
+        [Fact]
+        public async System.Threading.Tasks.Task Show_Share_UI_Request_NetStandard() =>
+            await Assert.ThrowsAsync<NotImplementedInReferenceAssemblyException>(() => DataTransfer.ShowShareUI(new ShareTextRequest()));
+    }
+}
