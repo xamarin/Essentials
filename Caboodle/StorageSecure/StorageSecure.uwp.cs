451be03dd51294151477f0a1b94dec97ff7dd964
Redth <jondick@gmail.com>
2018-03-20 15:52:07 -0400

    Simplify UWP code for Secure Storage

--- Caboodle/StorageSecure/StorageSecure.uwp.cs
+++ Caboodle/StorageSecure/StorageSecure.uwp.cs
@@ -24,7 +24,10 @@ namespace Microsoft.Caboodle
             {
                 var length = reader.ReadInt32();
                 var data = reader.ReadBytes(length);
-                var buffer = await StorageSecureProtectionExtensions.UnprotectAsync(data.AsBuffer()).ConfigureAwait(false);
+
+                var provider = new DataProtectionProvider();
+                var buffer = await provider.UnprotectAsync(data.AsBuffer()).AsTask().ConfigureAwait(false);
+
                 bytes_protected = buffer.ToArray();
             }
 
@@ -34,9 +37,11 @@ namespace Microsoft.Caboodle
         public static async Task SaveAsync(string data, string filename)
         {
             var file = await StorageFile.GetFileFromPathAsync(filename);
-
             var bytes = Encoding.UTF8.GetBytes(data);
-            var buffer = await StorageSecureProtectionExtensions.ProtectAsync(bytes.AsBuffer()).ConfigureAwait(false);
+
+            // LOCAL=user and LOCAL=machine do not require enterprise auth capability
+            var provider = new DataProtectionProvider("LOCAL=user");
+            var buffer = await provider.ProtectAsync(bytes.AsBuffer()).AsTask().ConfigureAwait(false);
             var bytes_protected = buffer.ToArray();
 
             using (var stream = await file.OpenStreamForWriteAsync().ConfigureAwait(false))
@@ -47,46 +52,6 @@ namespace Microsoft.Caboodle
             }
 
             return;
-        }
-    }
-
-    internal static class StorageSecureProtectionExtensions
-    {
-        /// <summary>
-        /// These providers do not require the enterprise authentication capability on either platform:
-        /// "LOCAL=user"
-        /// "LOCAL=machine"
-        /// </summary>
-        /// <param name="buffer"></param>
-        /// <param name="protectionDescriptor"></param>
-        /// <returns></returns>
-        public static async Task<IBuffer> ProtectAsync(this IBuffer buffer, string protectionDescriptor = "LOCAL=user")
-        {
-            if (buffer == null)
-            {
-                throw new ArgumentNullException(nameof(buffer));
-            }
-
-            if (protectionDescriptor == null)
-            {
-                throw new ArgumentNullException(nameof(protectionDescriptor));
-            }
-
-            var provider = new DataProtectionProvider(protectionDescriptor);
-
-            return await provider.ProtectAsync(buffer).AsTask().ConfigureAwait(false);
-        }
-
-        public static async Task<IBuffer> UnprotectAsync(this IBuffer buffer)
-        {
-            if (buffer == null)
-            {
-                throw new ArgumentNullException(nameof(buffer));
-            }
-
-            var provider = new DataProtectionProvider();
-
-            return await provider.UnprotectAsync(buffer).AsTask().ConfigureAwait(false);
         }
     }
 }
