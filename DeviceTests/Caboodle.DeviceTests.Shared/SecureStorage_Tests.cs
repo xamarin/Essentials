d73c92ea683bceffc726457f3f41c39f1c70f8ab
Redth <jondick@gmail.com>
2018-03-23 16:54:46 -0400

    SecureStorage: different test data for android
    This lets us test the both the API23+ and the < API23 implementations.

--- DeviceTests/Caboodle.DeviceTests.Shared/SecureStorage_Tests.cs
+++ DeviceTests/Caboodle.DeviceTests.Shared/SecureStorage_Tests.cs
@@ -5,6 +5,25 @@ namespace Microsoft.Caboodle.DeviceTests
 {
     public class SecureStorage_Tests
     {
+#if __ANDROID__
+        [Theory]
+        [InlineData("test.txt", "data", true)]
+        [InlineData("noextension", "data2", true)]
+        [InlineData("funny*&$%@!._/\\chars", "data3", true)]
+        [InlineData("test.txt2", "data2", false)]
+        [InlineData("noextension2", "data22", false)]
+        [InlineData("funny*&$%@!._/\\chars2", "data32", false)]
+        public async Task Saves_And_Loads(string key, string data, bool emulatePreApi23)
+        {
+            SecureStorage.AlwaysUseAsymmetricKeyStorage = emulatePreApi23;
+
+            await SecureStorage.SetAsync(key, data);
+
+            var c = await SecureStorage.GetAsync(key);
+
+            Assert.Equal(data, c);
+        }
+#else
         [Theory]
         [InlineData("test.txt", "data")]
         [InlineData("noextension", "data2")]
@@ -17,5 +36,6 @@ namespace Microsoft.Caboodle.DeviceTests
 
             Assert.Equal(data, c);
         }
+#endif
     }
 }
