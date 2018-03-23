2cf1e24a40045513eaf48072c369c47b869847ea
Redth <jondick@gmail.com>
2018-03-23 09:48:10 -0400

    SecureStorage: Clean up shared impl

--- Caboodle/SecureStorage/SecureStorage.shared.cs
+++ Caboodle/SecureStorage/SecureStorage.shared.cs
@@ -1,36 +1,16 @@
-﻿#if !NETSTANDARD1_0
-using System.Security.Cryptography;
-#endif
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Microsoft.Caboodle
 {
     public static partial class SecureStorage
     {
-        // md5(Caboodle.SecureStorage)
-        const string localDirName = "d2f74ffc823f1d9f015ee8edf1463498";
+        static string Alias =>
+            $"{AppInfo.PackageName}.caboodle";
 
         public static Task<string> GetAsync(string key)
-            => PlatformGetAsync(Md5Hash(key));
+            => PlatformGetAsync(key);
 
         public static Task SetAsync(string key, string value)
-            => PlatformSetAsync(Md5Hash(key), value);
-
-        internal static string Md5Hash(string input)
-        {
-#if NETSTANDARD1_0
-            throw new NotImplementedInReferenceAssemblyException();
-#else
-            var hash = new StringBuilder();
-            var md5provider = new MD5CryptoServiceProvider();
-            var bytes = md5provider.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-            for (var i = 0; i < bytes.Length; i++)
-                hash.Append(bytes[i].ToString("x2"));
-
-            return hash.ToString();
-#endif
-        }
+            => PlatformSetAsync(key, value);
     }
 }
