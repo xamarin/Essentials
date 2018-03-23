11af3ced2a065795e0062da1c1fa484ebcd3cfe4
Redth <jondick@gmail.com>
2018-03-23 09:46:16 -0400

    Added MD5Hash util function

--- Caboodle/Types/Shared/Utils.shared.cs
+++ Caboodle/Types/Shared/Utils.shared.cs
@@ -1,17 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
+#if !NETSTANDARD1_0
+using System.Security.Cryptography;
+#endif
 using System.Text;
 
 namespace Microsoft.Caboodle
 {
-    class Utils
+    internal class Utils
     {
-        public static Version ParseVersion(string version)
+        internal static Version ParseVersion(string version)
         {
             if (Version.TryParse(version, out var number))
                 return number;
 
             return new Version(0, 0);
+        }
+
+        internal static string Md5Hash(string input)
+        {
+#if NETSTANDARD1_0
+            throw new NotImplementedInReferenceAssemblyException();
+#else
+            var hash = new StringBuilder();
+            var md5provider = new MD5CryptoServiceProvider();
+            var bytes = md5provider.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            for (var i = 0; i < bytes.Length; i++)
+                hash.Append(bytes[i].ToString("x2"));
+
+            return hash.ToString();
+#endif
         }
     }
 }
