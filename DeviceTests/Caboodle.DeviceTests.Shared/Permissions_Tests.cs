2b471a92af570fcd5d789df9d25ed37e9d68b6a2
Redth <jondick@gmail.com>
2018-03-26 20:59:56 -0400

    Added device tests for permissions

---
+++ DeviceTests/Caboodle.DeviceTests.Shared/Permissions_Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Caboodle;
+using Xunit;
+
+namespace Caboodle.DeviceTests
+{
+    public class Permissions_Tests
+    {
+        [Theory]
+        [InlineData(PermissionType.Battery)]
+        [InlineData(PermissionType.NetworkState)]
+        [InlineData(PermissionType.LocationWhenInUse)]
+        internal void Ensure_Declared(PermissionType permission)
+        {
+            Permissions.EnsureDeclared(permission);
+        }
+
+        [Theory]
+        [InlineData(PermissionType.Battery, PermissionStatus.Granted)]
+        [InlineData(PermissionType.NetworkState, PermissionStatus.Granted)]
+        [InlineData(PermissionType.LocationWhenInUse, PermissionStatus.Granted)]
+        internal async Task Check_Status(PermissionType permission, PermissionStatus expectedStatus)
+        {
+            var status = await Permissions.CheckStatusAsync(permission);
+
+            Assert.Equal(expectedStatus, status);
+        }
+
+        [Theory]
+        [InlineData(PermissionType.Battery, PermissionStatus.Granted)]
+        [InlineData(PermissionType.NetworkState, PermissionStatus.Granted)]
+        internal async Task Request(PermissionType permission, PermissionStatus expectedStatus)
+        {
+            var status = await Permissions.CheckStatusAsync(permission);
+
+            Assert.Equal(expectedStatus, status);
+        }
+    }
+}
