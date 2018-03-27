98478ccbb822b5f030c6d388869fe2eccd310e41
James Montemagno <james.montemagno@gmail.com>
2018-03-27 13:55:42 -0700

    Add xunit tests

--- Caboodle.Tests/Compass_Tests.cs
+++ Caboodle.Tests/Compass_Tests.cs
@@ -6,6 +6,15 @@ namespace Caboodle.Tests
 {
     public class Compass_Tests
     {
+        public Compass_Tests()
+        {
+            Compass.DisposeToken();
+        }
+
+        [Fact]
+        public void IsSupported_On_NetStandard() =>
+            Assert.Throws<NotImplementedInReferenceAssemblyException>(() => Compass.IsSupported);
+
         [Fact]
         public void Monitor_Null_Handler_On_NetStandard() =>
             Assert.Throws<ArgumentNullException>(() => Compass.Monitor(SensorSpeed.Normal, null));
@@ -17,5 +26,28 @@ namespace Caboodle.Tests
         [Fact]
         public void IsMonitoring_Default_On_NetStandard() =>
             Assert.False(Compass.IsMonitoring);
+
+        [Fact]
+        public void IsMonitoring_NetStandard()
+        {
+            Compass.CreateToken();
+            Assert.True(Compass.IsMonitoring);
+        }
+
+        [Fact]
+        public void Dispose_Token_NetStandard()
+        {
+            Compass.CreateToken();
+            Compass.DisposeToken();
+            Assert.Null(Compass.MonitorCTS);
+        }
+
+        [Fact]
+        public void Stop_Monitor_NetStandard()
+        {
+            Compass.CreateToken();
+            Compass.StopMonitor();
+            Assert.False(Compass.IsMonitoring);
+        }
     }
 }
