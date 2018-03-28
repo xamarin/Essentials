ed4c4211bd9ca35edb3d9946aa600f1c6a043415
Matthew Leibowitz <mattleibow@live.com>
2018-03-29 00:32:51 +0200

    Update the tests - removed the emil tests as this will leave the current app

--- Caboodle.Tests/Email_Tests.cs
+++ Caboodle.Tests/Email_Tests.cs
@@ -5,21 +5,5 @@ namespace Caboodle.Tests
 {
     public class Email_Tests
     {
-        [Fact]
-        public void Email_OnSendSuccess_Event_On_NetStandard() =>
-            Assert.Throws<NotImplementedInReferenceAssemblyException>(() => Email.OnSendSuccess += Email_OnSendSuccess);
-
-        [Fact]
-        public void Email_OnSendError_Event_On_NetStandard() =>
-            Assert.Throws<NotImplementedInReferenceAssemblyException>(() => Email.OnSendError += Email_OnSendSuccess);
-
-
-        void Email_OnSendSuccess(EmailSendEventArgs e)
-        {
-        }
-
-        void Email_OnSendError(EmailSendEventArgs e)
-        {
-        }
     }
 }
