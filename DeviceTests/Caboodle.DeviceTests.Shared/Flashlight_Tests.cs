4c58c408e60628184a131782aa1bf12d1b502fe0
Redth <jondick@gmail.com>
2018-03-28 14:53:14 -0400

    Added flashlight device tests

---
+++ DeviceTests/Caboodle.DeviceTests.Shared/Flashlight_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Caboodle;
+using Xunit;
+
+namespace Caboodle.DeviceTests
+{
+    public class Flashlight_Tests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task Turn_On_Off(bool oldCameraApi)
+        {
+#if __ANDROID__
+            // API 23+ we need user interaction for camera permission
+            // can't really test so easily on device.
+            if (Platform.HasApiLevel(Android.OS.BuildVersionCodes.M))
+                return;
+
+            Flashlight.AlwaysUseCameraApi = oldCameraApi;
+#endif
+            await Flashlight.TurnOnAsync();
+            await Flashlight.TurnOffAsync();
+        }
+    }
+}
