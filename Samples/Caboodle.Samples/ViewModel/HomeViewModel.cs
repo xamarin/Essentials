135500c8a0527c6e85cf05ed974fb2191702787a
Matthew Leibowitz <mattleibow@live.com>
2018-03-29 18:53:11 +0200

    Added the geolocation sample

--- Samples/Caboodle.Samples/ViewModel/HomeViewModel.cs
+++ Samples/Caboodle.Samples/ViewModel/HomeViewModel.cs
@@ -20,6 +20,7 @@ namespace Caboodle.Samples.ViewModel
                 new SampleItem("File System", typeof(FileSystemPage), "Easily save files to app data."),
                 new SampleItem("Flashlight", typeof(FlashlightPage), "A simple way to turn the flashlight on/off."),
                 new SampleItem("Geocoding", typeof(GeocodingPage), "Easily geocode and reverse geocoding."),
+                new SampleItem("Geolocation", typeof(GeolocationPage), "Quickly get the current location."),
                 new SampleItem("Phone Dialer", typeof(PhoneDialerPage), "Easily open phone dialer."),
                 new SampleItem("Preferences", typeof(PreferencesPage), "Quickly and easily add persistent preferences."),
                 new SampleItem("Screen Lock", typeof(ScreenLockPage), "Keep the device screen awake."),
