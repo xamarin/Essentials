2414d741a1cb497672c79f2fa5496ac9fd7e1fda
Matthew Leibowitz <mattleibow@live.com>
2018-04-12 00:32:17 +0200

    Added hardware traits to the tests to avoid evil code

--- DeviceTests/DeviceTests.Shared/SecureStorage_Tests.cs
+++ DeviceTests/DeviceTests.Shared/SecureStorage_Tests.cs
@@ -4,6 +4,8 @@ using Xunit;
 
 namespace DeviceTests
 {
+    // TEST NOTES:
+    //   - TODO: the iOS simulators have issues with the way we set up our tests... we need to fix that
     public class SecureStorage_Tests
     {
         [Theory]
@@ -13,13 +15,13 @@ namespace DeviceTests
         [InlineData("test.txt2", "data2", false)]
         [InlineData("noextension2", "data22", false)]
         [InlineData("funny*&$%@!._/\\chars2", "data32", false)]
+#if __IOS__
+        [Trait(Traits.DeviceType, Traits.DeviceTypes.Physical)]
+#endif
         public async Task Saves_And_Loads(string key, string data, bool emulatePreApi23)
         {
 #if __ANDROID__
             SecureStorage.AlwaysUseAsymmetricKeyStorage = emulatePreApi23;
-#elif __IOS__
-            if (Utils.IsiOSSimulator)
-                return;
 #endif
 
             await SecureStorage.SetAsync(key, data);
