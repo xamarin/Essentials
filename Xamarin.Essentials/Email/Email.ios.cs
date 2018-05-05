9c9b4265e9d5af9f16b06c5205c88b8faefad930
Matthew Leibowitz <mattleibow@live.com>
2018-05-05 04:24:09 +0200

    A bit of the fixes for emails on iOS. Relates to #224

--- Xamarin.Essentials/Email/Email.ios.cs
+++ Xamarin.Essentials/Email/Email.ios.cs
@@ -1,14 +1,37 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Foundation;
 using MessageUI;
+using UIKit;
 
 namespace Xamarin.Essentials
 {
     public static partial class Email
     {
         internal static bool IsComposeSupported
-            => MFMailComposeViewController.CanSendMail;
+        {
+            get
+            {
+                var can = MFMailComposeViewController.CanSendMail;
+                if (!can)
+                {
+                    var url = NSUrl.FromString("mailto://?to=test@xamarin.com");
+                    NSRunLoop.Main.InvokeOnMainThread(() => can = UIApplication.SharedApplication.CanOpenUrl(url));
+                }
+                return can;
+            }
+        }
 
         static Task PlatformComposeAsync(EmailMessage message)
+        {
+            if (MFMailComposeViewController.CanSendMail)
+                return ComposeWithMailCompose(message);
+            else
+                return ComposeWithUrl(message);
+        }
+
+        static Task ComposeWithMailCompose(EmailMessage message)
         {
             // do this first so we can throw as early as possible
             var parentController = Platform.GetCurrentViewController();
@@ -35,6 +58,34 @@ namespace Xamarin.Essentials
             };
             parentController.PresentViewController(controller, true, null);
 
+            return tcs.Task;
+        }
+
+        static Task ComposeWithUrl(EmailMessage message)
+        {
+            if (message != null && message.BodyFormat != EmailBodyFormat.PlainText)
+                throw new FeatureNotSupportedException("iOS can only compose plain text email messages when a default email account is not set up.");
+
+            var parts = new List<string>();
+            if (!string.IsNullOrEmpty(message?.Body))
+                parts.Add("body=" + Uri.EscapeUriString(message.Body));
+            if (!string.IsNullOrEmpty(message?.Subject))
+                parts.Add("subject=" + Uri.EscapeUriString(message.Subject));
+            if (message?.To.Count > 0)
+                parts.Add("to=" + string.Join(",", message.To));
+            if (message?.Cc.Count > 0)
+                parts.Add("cc=" + string.Join(",", message.Cc));
+            if (message?.Bcc.Count > 0)
+                parts.Add("bcc=" + string.Join(",", message.Bcc));
+
+            var url = "mailto://?" + string.Join("&", parts);
+
+            var tcs = new TaskCompletionSource<bool>();
+            NSRunLoop.Main.InvokeOnMainThread(() =>
+            {
+                var nsurl = NSUrl.FromString(url);
+                UIApplication.SharedApplication.OpenUrl(nsurl, (NSDictionary)null, r => tcs.TrySetResult(r));
+            });
             return tcs.Task;
         }
     }
