99aa4f7ed52354e8e64f87e958fa2c3e41489ba3
Matthew Leibowitz <mattleibow@live.com>
2018-05-11 22:42:47 +0200

    Added the Browser API and some basic permissions checking

--- Xamarin.Essentials/Browser/Browser.tizen.cs
+++ Xamarin.Essentials/Browser/Browser.tizen.cs
@@ -1,11 +1,31 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Tizen.Applications;
 
 namespace Xamarin.Essentials
 {
     public static partial class Browser
     {
-        static Task PlatformOpenAsync(Uri uri, BrowserLaunchType launchType) =>
-            throw new NotImplementedInReferenceAssemblyException();
+        static Task PlatformOpenAsync(Uri uri, BrowserLaunchType launchType)
+        {
+            if (uri == null)
+                throw new ArgumentNullException(nameof(uri));
+
+            Permissions.EnsureDeclared(PermissionType.LaunchApp);
+
+            var appControl = new AppControl
+            {
+                Operation = AppControlOperations.View,
+                Uri = uri.AbsoluteUri
+            };
+
+            var hasMatches = AppControl.GetMatchedApplicationIds(appControl).Any();
+
+            if (hasMatches)
+                AppControl.SendLaunchRequest(appControl);
+
+            return Task.FromResult(hasMatches);
+        }
     }
 }
--- Xamarin.Essentials/Permissions/Permissions.tizen.cs
+++ Xamarin.Essentials/Permissions/Permissions.tizen.cs
@@ -1,16 +1,48 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Xamarin.Essentials
 {
     internal static partial class Permissions
     {
-        static void PlatformEnsureDeclared(PermissionType permission) =>
-            throw new NotImplementedInReferenceAssemblyException();
+        static void PlatformEnsureDeclared(PermissionType permission)
+        {
+            var tizenPrivileges = permission.ToTizenPrivileges();
+
+            // No actual tizen privileges required here, just return
+            if (tizenPrivileges == null || !tizenPrivileges.Any())
+                return;
+
+            var package = Platform.CurrentPackage;
+            foreach (var priv in tizenPrivileges)
+            {
+                if (!package.Privileges.Contains(priv))
+                    throw new PermissionException($"You need to declare the privilege: `{priv}` in your tizen-manifest.xml");
+            }
+        }
 
         static Task<PermissionStatus> PlatformCheckStatusAsync(PermissionType permission) =>
             throw new NotImplementedInReferenceAssemblyException();
 
         static Task<PermissionStatus> PlatformRequestAsync(PermissionType permission) =>
             throw new NotImplementedInReferenceAssemblyException();
+    }
+
+    static class PermissionTypeExtensions
+    {
+        internal static IEnumerable<string> ToTizenPrivileges(this PermissionType permissionType)
+        {
+            var privileges = new List<string>();
+
+            switch (permissionType)
+            {
+                case PermissionType.LaunchApp:
+                    privileges.Add("http://tizen.org/privilege/appmanager.launch");
+                    break;
+            }
+
+            return privileges;
+        }
     }
 }
