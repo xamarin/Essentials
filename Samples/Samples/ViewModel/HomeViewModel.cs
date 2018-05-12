46bf5cb694e726d63a63c0f448468fa5e8a07543
Charles Petzold <chape@microsoft.com>
2018-05-12 12:02:41 -0400

    Add Orientation Sensor

--- Samples/Samples/ViewModel/HomeViewModel.cs
+++ Samples/Samples/ViewModel/HomeViewModel.cs
@@ -28,8 +28,8 @@ namespace Samples.ViewModel
                     "📏",
                     "All Sensors",
                     typeof(AllSensorsPage),
-                    "Have a look at the accelerometer, compass, gyroscope and magnetometer.",
-                    new[] { "accelerometer", "compass", "gyroscope", "magnetometer", "sensors", "hardware", "device" }),
+                    "Have a look at the accelerometer, compass, gyroscope, magnetometer, and orientationsensor.",
+                    new[] { "accelerometer", "compass", "gyroscope", "magnetometer", "orientationsensor", "sensors", "hardware", "device" }),
                 new SampleItem(
                     "📦",
                     "App Info",
@@ -120,6 +120,12 @@ namespace Samples.ViewModel
                     typeof(MagnetometerPage),
                     "Detect device's orientation relative to Earth's magnetic field.",
                     new[] { "compass", "magnetometer", "sensors", "hardware", "device" }),
+                new SampleItem(
+                    "📏",
+                    "OrientationSensor",
+                    typeof(OrientationSensorPage),
+                    "Retrieve orientation of the device in 3D space.",
+                    new[] { "orientationsensor", "sensors", "hardware", "device" }),
                 new SampleItem(
                     "📞",
                     "Phone Dialer",
