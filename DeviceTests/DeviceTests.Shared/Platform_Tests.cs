dbdd4ee68762ae1deb1fdb62ac02abfc5e3a2960
James Montemagno <james.montemagno@gmail.com>
2018-06-04 10:45:56 -0700

    Add platform tests for main thread

---
+++ DeviceTests/DeviceTests.Shared/Platform_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xunit;
+
+namespace DeviceTests
+{
+    public class Platform_Tests
+    {
+        [Fact]
+        public Task IsOnMainThread()
+        {
+            return Utils.OnMainThread(() =>
+            {
+                Assert.True(Platform.IsMainThread);
+            });
+        }
+    }
+}
