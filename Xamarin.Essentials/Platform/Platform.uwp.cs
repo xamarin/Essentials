059b1fa6b0649f0ac171135e196abacdddd4c09c
Matthew Leibowitz <mattleibow@live.com>
2018-06-14 17:12:13 +0200

    Properly check for thread access on UWP

--- Xamarin.Essentials/Platform/Platform.uwp.cs
+++ Xamarin.Essentials/Platform/Platform.uwp.cs
@@ -7,13 +7,13 @@ namespace Xamarin.Essentials
     public static partial class Platform
     {
         static bool PlatformIsMainThread =>
-            CoreApplication.MainView.CoreWindow.Dispatcher == null;
+            CoreApplication.MainView.CoreWindow.Dispatcher.HasThreadAccess;
 
         static void PlatformBeginInvokeOnMainThread(Action action)
         {
             var dispatcher = CoreApplication.MainView.CoreWindow.Dispatcher;
 
-            if (dispatcher != null)
+            if (!dispatcher.HasThreadAccess)
                 dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action()).WatchForError();
             else
                 action();
