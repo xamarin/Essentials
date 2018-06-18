61472633a2255d7faafccc069ab952e5b37cea5a
sung-su.kim <sung-su.kim@samsung.com>
2018-06-18 16:59:34 +0900

    Added the Filesystem API

--- Xamarin.Essentials/FileSystem/FileSystem.tizen.cs
+++ Xamarin.Essentials/FileSystem/FileSystem.tizen.cs
@@ -1,17 +1,26 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using Tizen.Applications;
 
 namespace Xamarin.Essentials
 {
     public static partial class FileSystem
     {
         static string PlatformCacheDirectory
-            => throw new NotImplementedInReferenceAssemblyException();
+            => Application.Current.DirectoryInfo.Cache;
 
         static string PlatformAppDataDirectory
-            => throw new NotImplementedInReferenceAssemblyException();
+            => Application.Current.DirectoryInfo.Data;
 
         static Task<Stream> PlatformOpenAppPackageFileAsync(string filename)
-             => throw new NotImplementedInReferenceAssemblyException();
+        {
+            if (string.IsNullOrWhiteSpace(filename))
+                throw new ArgumentNullException(nameof(filename));
+
+            filename = filename.Replace('\\', Path.DirectorySeparatorChar);
+            Stream fs = File.OpenRead(Path.Combine(Application.Current.DirectoryInfo.Resource, filename));
+            return Task.FromResult(fs);
+        }
     }
 }
