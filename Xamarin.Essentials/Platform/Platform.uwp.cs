7c9ac8efbf2f83c31646281c3e09269ef16e805d
Matthew Leibowitz <mattleibow@live.com>
2018-06-18 18:07:06 +0200

    Do some null checks with UWP

--- Xamarin.Essentials/Platform/Platform.uwp.cs
+++ Xamarin.Essentials/Platform/Platform.uwp.cs
@@ -7,11 +7,14 @@ namespace Xamarin.Essentials
     public static partial class Platform
     {
         static bool PlatformIsMainThread =>
-            CoreApplication.MainView.CoreWindow.Dispatcher.HasThreadAccess;
+            CoreApplication.MainView.CoreWindow?.Dispatcher?.HasThreadAccess ?? false;
 
         static void PlatformBeginInvokeOnMainThread(Action action)
         {
-            var dispatcher = CoreApplication.MainView.CoreWindow.Dispatcher;
+            var dispatcher = CoreApplication.MainView.CoreWindow?.Dispatcher;
+
+            if (dispatcher == null)
+                throw new InvalidOperationException("Unable to find main thread.");
 
             if (!dispatcher.HasThreadAccess)
                 dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action()).WatchForError();
