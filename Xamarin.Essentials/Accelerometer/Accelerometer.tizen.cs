41a1464f25eb3c098e0db90bdb42701e805bc345
Seunghyun Choi <sh4682.choi@samsung.com>
2018-06-18 13:46:45 +0900

    Added the Acceleromerter API

--- Xamarin.Essentials/Accelerometer/Accelerometer.tizen.cs
+++ Xamarin.Essentials/Accelerometer/Accelerometer.tizen.cs
@@ -1,14 +1,59 @@
-﻿namespace Xamarin.Essentials
+﻿using Tizen.Sensor;
+using TizenAccelerometer = Tizen.Sensor.Accelerometer;
+
+namespace Xamarin.Essentials
 {
     public static partial class Accelerometer
     {
-        internal static bool IsSupported =>
-            throw new NotImplementedInReferenceAssemblyException();
+        internal const uint FastestInterval = 20;
+        internal const uint GameInterval = 40;
+        internal const uint UiInterval = 80;
 
-        static void PlatformStart(SensorSpeed sensorSpeed) =>
-            throw new NotImplementedInReferenceAssemblyException();
+        static TizenAccelerometer sensor;
 
-        static void PlatformStop() =>
-            throw new NotImplementedInReferenceAssemblyException();
+        internal static bool IsSupported
+        {
+            get
+            {
+                return TizenAccelerometer.IsSupported;
+            }
+        }
+
+        static void PlatformStart(SensorSpeed sensorSpeed)
+        {
+            sensor = new TizenAccelerometer();
+
+            uint interval = 0;
+
+            switch (sensorSpeed)
+            {
+                case SensorSpeed.Fastest:
+                    interval = FastestInterval;
+                    break;
+                case SensorSpeed.Game:
+                    interval = GameInterval;
+                    break;
+                case SensorSpeed.Ui:
+                    interval = UiInterval;
+                    break;
+            }
+
+            sensor.Interval = interval;
+            sensor.DataUpdated += DataUpdated;
+            sensor.Start();
+        }
+
+        static void PlatformStop()
+        {
+            sensor.DataUpdated -= DataUpdated;
+            sensor.Stop();
+            sensor.Dispose();
+        }
+
+        static void DataUpdated(object sender, AccelerometerDataUpdatedEventArgs e)
+        {
+            var data = new AccelerometerData(e.X, e.Y, e.Z);
+            OnChanged(data);
+        }
     }
 }
