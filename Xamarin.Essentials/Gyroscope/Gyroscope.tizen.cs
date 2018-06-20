41cadeb4c5dee22788a0596fcb4ce4585b5c3161
Seunghyun Choi <sh4682.choi@samsung.com>
2018-06-20 13:32:04 +0900

    Added the Gyroscope API

--- Xamarin.Essentials/Gyroscope/Gyroscope.tizen.cs
+++ Xamarin.Essentials/Gyroscope/Gyroscope.tizen.cs
@@ -1,14 +1,50 @@
-﻿namespace Xamarin.Essentials
+﻿using Tizen.Sensor;
+using TizenGyroscope = Tizen.Sensor.Gyroscope;
+
+namespace Xamarin.Essentials
 {
     public static partial class Gyroscope
     {
+        internal const uint GameInterval = 20;
+        internal const uint UiInterval = 60;
+
+        internal static TizenGyroscope DefaultSensor =>
+            (TizenGyroscope)Platform.GetDefaultSensor(SensorType.Gyroscope);
+
         internal static bool IsSupported =>
-            throw new NotImplementedInReferenceAssemblyException();
+            TizenGyroscope.IsSupported;
 
-        internal static void PlatformStart(SensorSpeed sensorSpeed) =>
-            throw new NotImplementedInReferenceAssemblyException();
+        internal static void PlatformStart(SensorSpeed sensorSpeed)
+        {
+            uint interval = 0;
 
-        internal static void PlatformStop() =>
-            throw new NotImplementedInReferenceAssemblyException();
+            switch (sensorSpeed)
+            {
+                case SensorSpeed.Fastest:
+                    interval = (uint)DefaultSensor.MinInterval;
+                    break;
+                case SensorSpeed.Game:
+                    interval = GameInterval;
+                    break;
+                case SensorSpeed.Ui:
+                    interval = UiInterval;
+                    break;
+            }
+
+            DefaultSensor.Interval = interval;
+            DefaultSensor.DataUpdated += DataUpdated;
+            DefaultSensor.Start();
+        }
+
+        internal static void PlatformStop()
+        {
+            DefaultSensor.DataUpdated -= DataUpdated;
+            DefaultSensor.Stop();
+        }
+
+        static void DataUpdated(object sender, GyroscopeDataUpdatedEventArgs e)
+        {
+            OnChanged(new GyroscopeData(e.X, e.Y, e.Z));
+        }
     }
 }
