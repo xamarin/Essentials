27d017369bf0c0c8a842ea710868ce43cf8c72bc
Seunghyun Choi <sh4682.choi@samsung.com>
2018-06-20 13:51:41 +0900

    Added the Magnetometer API

--- Xamarin.Essentials/Magnetometer/Magnetometer.tizen.cs
+++ Xamarin.Essentials/Magnetometer/Magnetometer.tizen.cs
@@ -1,14 +1,50 @@
-﻿namespace Xamarin.Essentials
+﻿using Tizen.Sensor;
+using TizenMagnetometer = Tizen.Sensor.Magnetometer;
+
+namespace Xamarin.Essentials
 {
     public static partial class Magnetometer
     {
+        internal const uint GameInterval = 20;
+        internal const uint UiInterval = 60;
+
+        internal static TizenMagnetometer DefaultSensor =>
+            (TizenMagnetometer)Platform.GetDefaultSensor(SensorType.Magnetometer);
+
         internal static bool IsSupported =>
-            throw new NotImplementedInReferenceAssemblyException();
+            TizenMagnetometer.IsSupported;
 
-        internal static void PlatformStart(SensorSpeed sensorSpeed) =>
-            throw new NotImplementedInReferenceAssemblyException();
+        internal static void PlatformStart(SensorSpeed sensorSpeed)
+        {
+            uint interval = 0;
 
-        internal static void PlatformStop() =>
-            throw new NotImplementedInReferenceAssemblyException();
+            switch (sensorSpeed)
+            {
+                case SensorSpeed.Fastest:
+                    interval = (uint)DefaultSensor.MinInterval;
+                    break;
+                case SensorSpeed.Game:
+                    interval = GameInterval;
+                    break;
+                case SensorSpeed.Ui:
+                    interval = UiInterval;
+                    break;
+            }
+
+            DefaultSensor.Interval = interval;
+            DefaultSensor.DataUpdated += DataUpdated;
+            DefaultSensor.Start();
+        }
+
+        internal static void PlatformStop()
+        {
+            DefaultSensor.DataUpdated -= DataUpdated;
+            DefaultSensor.Stop();
+        }
+
+        static void DataUpdated(object sender, MagnetometerDataUpdatedEventArgs e)
+        {
+            OnChanged(new MagnetometerData(e.X, e.Y, e.Z));
+        }
     }
 }
