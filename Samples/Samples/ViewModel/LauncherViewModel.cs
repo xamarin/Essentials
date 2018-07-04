8639839152733256406066781ca564d12320af30
Schilli, Niklas <schilli@ams-berlin.de>
2018-07-04 18:54:51 +0200

    Added sample launcher page and viewmodel.

---
+++ Samples/Samples/ViewModel/LauncherViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Samples.ViewModel
+{
+    public class LauncherViewModel : BaseViewModel
+    {
+        public string LaunchUri { get; set; }
+
+        public ICommand LaunchCommand { get; }
+
+        public ICommand CanLaunchCommand { get; }
+
+        public LauncherViewModel()
+        {
+            LaunchCommand = new Command(OnLaunch);
+            CanLaunchCommand = new Command(CanLaunch);
+        }
+
+        async void OnLaunch()
+        {
+            await Launcher.OpenAsync(LaunchUri);
+        }
+
+        async void CanLaunch()
+        {
+            var canBeLaunched = await Launcher.CanOpenAsync(LaunchUri);
+            await DisplayAlertAsync($"Uri {LaunchUri} can be Launched: {canBeLaunched}");
+        }
+    }
+}
