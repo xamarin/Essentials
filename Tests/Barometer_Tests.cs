ab202af3c2e0c5766bab51089b75d70327286dd1
Schilli, Niklas <schilli@ams-berlin.de>
2018-07-08 23:54:47 +0200

    Added more unit tests

---
+++ Tests/Barometer_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xunit;
+
+namespace Tests
+{
+    public class Barometer_Tests
+    {
+        [Fact]
+        public void IsSupported_On_NetStandard() =>
+            Assert.Throws<NotImplementedInReferenceAssemblyException>(() => Barometer.IsSupported);
+
+        [Fact]
+        public void Monitor_On_NetStandard() =>
+            Assert.Throws<NotImplementedInReferenceAssemblyException>(() => Barometer.Start());
+
+        [Fact]
+        public void IsMonitoring_Default_On_NetStandard() =>
+            Assert.False(Barometer.IsMonitoring);
+    }
+}
