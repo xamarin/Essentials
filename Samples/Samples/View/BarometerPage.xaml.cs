2c1705b4c904afc76b7e0a9b40c4541371b0b5d5
Schilli, Niklas <schilli@ams-berlin.de>
2018-07-08 23:55:04 +0200

    Added samples

---
+++ Samples/Samples/View/BarometerPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Samples.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BarometerPage : BasePage
+    {
+        public BarometerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Samples/Samples/ViewModel/BarometerViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Samples.ViewModel
+{
+    public class BarometerViewModel : BaseViewModel
+    {
+        bool barometerIsActive;
+        double barometer;
+
+        public BarometerViewModel()
+        {
+            StartBarometerComand = new Command(OnStartBarometer);
+            StopBaromterCommand = new Command(OnStopBarometer);
+        }
+
+        public ICommand StartBarometerComand { get; }
+
+        public ICommand StopBaromterCommand { get; }
+
+        public bool BarometerIsActive
+        {
+            get => barometerIsActive;
+            set => SetProperty(ref barometerIsActive, value);
+        }
+
+        public double BaromterValue
+        {
+            get => barometer;
+            set => SetProperty(ref barometer, value);
+        }
+
+        public override void OnDisappearing()
+        {
+            OnStopBarometer();
+
+            base.OnDisappearing();
+        }
+
+        async void OnStartBarometer()
+        {
+            try
+            {
+                Barometer.ReadingChanged += OnBarometerReadingChanged;
+
+                Barometer.Start();
+                BarometerIsActive = true;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync($"Unable to start compass: {ex.Message}");
+            }
+        }
+
+        void OnBarometerReadingChanged(BarometerChangedEventArgs e)
+        {
+            BaromterValue = e.BarometerData.Pressure;
+        }
+
+        void OnStopBarometer()
+        {
+            BarometerIsActive = false;
+            Barometer.Stop();
+            Barometer.ReadingChanged -= OnBarometerReadingChanged;
+        }
+    }
+}
