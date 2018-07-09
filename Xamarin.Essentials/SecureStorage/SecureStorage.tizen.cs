f10f0e129a995891d3fa016669ca73408405e00d
sung-su.kim <sung-su.kim@samsung.com>
2018-07-09 18:10:58 +0900

    Added the SecureStorage API

--- Xamarin.Essentials/SecureStorage/SecureStorage.tizen.cs
+++ Xamarin.Essentials/SecureStorage/SecureStorage.tizen.cs
@@ -1,13 +1,46 @@
-﻿using System.Threading.Tasks;
+﻿using System.Text;
+using System.Threading.Tasks;
+using Tizen.Security.SecureRepository;
 
 namespace Xamarin.Essentials
 {
     public partial class SecureStorage
     {
-        static Task<string> PlatformGetAsync(string key) =>
-            throw new NotImplementedInReferenceAssemblyException();
+        static Task<string> PlatformGetAsync(string key)
+        {
+            try
+            {
+                return Task.FromResult(Encoding.UTF8.GetString(DataManager.Get(key, null)));
+            }
+            catch
+            {
+                Tizen.Log.Error(Platform.CurrentPackage.Label, "Failed to load data.");
+                throw;
+            }
+        }
 
-        static Task PlatformSetAsync(string key, string data) =>
-            throw new NotImplementedInReferenceAssemblyException();
+        static Task PlatformSetAsync(string key, string data)
+        {
+            try
+            {
+                DataManager.RemoveAlias(key);
+            }
+            catch
+            {
+                Tizen.Log.Info(Platform.CurrentPackage.Label, "No save data.");
+            }
+
+            try
+            {
+                DataManager.Save(key, Encoding.UTF8.GetBytes(data), new Policy());
+            }
+            catch
+            {
+                Tizen.Log.Error(Platform.CurrentPackage.Label, "Failed to save data.");
+                throw;
+            }
+
+            return Task.CompletedTask;
+        }
     }
 }
