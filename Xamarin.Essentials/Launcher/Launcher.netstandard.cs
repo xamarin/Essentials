c3e725086cfb6a670d83572c469a223061d2329e
Schilli, Niklas <schilli@ams-berlin.de>
2018-07-09 19:31:51 +0200

    Removed dead code

--- Xamarin.Essentials/Launcher/Launcher.netstandard.cs
+++ Xamarin.Essentials/Launcher/Launcher.netstandard.cs
@@ -5,11 +5,7 @@ namespace Xamarin.Essentials
 {
     public static partial class Launcher
     {
-        static Task<bool> PlatformCanOpenAsync(string uri) => throw new NotImplementedInReferenceAssemblyException();
-
         static Task<bool> PlatformCanOpenAsync(Uri uri) => throw new NotImplementedInReferenceAssemblyException();
-
-        static Task PlatformOpenAsync(string uri) => throw new NotImplementedInReferenceAssemblyException();
 
         static Task PlatformOpenAsync(Uri uri) => throw new NotImplementedInReferenceAssemblyException();
     }
