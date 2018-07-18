bfccc99feee350f17f96113da79a9ca94414d724
Redth <jondick@gmail.com>
2018-07-18 14:32:29 -0400

    Allow for unicode in host names and escape query
    Previously we were passing the the url as is to each platform and letting it deal with how to parse into the platform specific url.
    
    This handles unicode domain names (IDN Mapping aka PunyCode) for the domain, but also reconstructs the url based on the parsed System.Uri from the original string, and so the PathAndQuery will be the % escaped version which will account for unicode characters in it as well.
    
    Finally, on iOS and Android we are using AbsoluteUri to end up with the fully IDN mapped and % encoded URL to pass to the system URL types.

--- Xamarin.Essentials/Browser/Browser.shared.cs
+++ Xamarin.Essentials/Browser/Browser.shared.cs
@@ -21,8 +21,15 @@ namespace Xamarin.Essentials
         public static Task OpenAsync(Uri uri) =>
           OpenAsync(uri, BrowserLaunchType.SystemPreferred);
 
-        public static Task OpenAsync(Uri uri, BrowserLaunchType launchType) =>
-            PlatformOpenAsync(uri, launchType);
+        public static Task OpenAsync(Uri uri, BrowserLaunchType launchType)
+        {
+#if NETSTANDARD1_0
+            return PlatformOpenAsync(uri, launchType);
+#else
+            var idn = new System.Globalization.IdnMapping();
+            return PlatformOpenAsync(new Uri(uri.Scheme + "://" + idn.GetAscii(uri.DnsSafeHost) + uri.PathAndQuery), launchType);
+#endif
+        }
     }
 
     public enum BrowserLaunchType
