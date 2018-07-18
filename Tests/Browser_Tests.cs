ef2f2f1fd8b2e257de4710e2b9ba2b09e74d7c27
Redth <jondick@gmail.com>
2018-07-18 16:34:44 -0400

    Add uri escaping tests

--- Tests/Browser_Tests.cs
+++ Tests/Browser_Tests.cs
@@ -22,5 +22,19 @@ namespace Tests
         [Fact]
         public async Task Open_Uri_Launch_NetStandard() =>
             await Assert.ThrowsAsync<NotImplementedInReferenceAssemblyException>(() => Browser.OpenAsync(new Uri("http://xamarin.com"), BrowserLaunchType.SystemPreferred));
+
+        [Theory]
+        [InlineData("https://xamarin.com", "https://xamarin.com")]
+        [InlineData("http://xamarin.com", "http://xamarin.com")]
+        [InlineData("https://mañana.com", "https://xn--maana-pta.com")]
+        [InlineData("http://mañana.com", "http://xn--maana-pta.com")]
+        [InlineData("https://mañana.com/?test=mañana", "https://xn--maana-pta.com/?test=ma%C3%B1ana")]
+        [InlineData("http://mañana.com/?test=mañana", "http://xn--maana-pta.com/?test=ma%C3%B1ana")]
+        public void Escape_Uri(string uri, string escaped)
+        {
+            var escapedUri = Browser.EscapeUri(new Uri(uri));
+
+            Assert.Equal(escaped, escapedUri.AbsoluteUri.TrimEnd('/'));
+        }
     }
 }
