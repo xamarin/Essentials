e054abd191300f3bf795deaf143c2187cca4c54f
Schilli, Niklas <schilli@ams-berlin.de>
2018-07-24 22:18:36 +0200

    Added unit tests

---
+++ Tests/AppInfo_Tests.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Essentials;
+using Xunit;
+
+namespace Tests
+{
+    public class AppInfo_Tests
+    {
+        void AppStateDelegate(AppStateChangedEventArgs args)
+        {
+        }
+
+        [Fact]
+        public void State_On_NetStandard()
+            => Assert.Throws<NotImplementedInReferenceAssemblyException>(() => AppInfo.State);
+
+        [Fact]
+        public void AppStateChanged_Subscribe_Unsubscribe_On_NetStandard()
+        {
+            Assert.Throws<NotImplementedInReferenceAssemblyException>(
+                () => AppInfo.AppStateChanged += AppStateDelegate);
+            Assert.Throws<NotImplementedInReferenceAssemblyException>(
+                () => AppInfo.AppStateChanged -= AppStateDelegate);
+        }
+    }
+}
