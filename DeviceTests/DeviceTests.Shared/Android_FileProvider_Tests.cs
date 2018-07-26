6695cb9c81bcd97c911ff29b7c161c738a22e914
Redth <jondick@gmail.com>
2018-07-26 10:22:23 -0400

    Add a user interaction test for File Provider

---
+++ DeviceTests/DeviceTests.Shared/Android_FileProvider_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xamarin.Essentials;
+using System.IO;
+
+namespace DeviceTests.Shared
+{
+#if __ANDROID__
+    public class Android_FileProvider_Tests
+    {
+        [Trait(Traits.InteractionType, Traits.InteractionTypes.Human)]
+        public void Share_Simple_Text_File_Test()
+        {
+            const string TestStr = "Test Text File";
+
+            // Save a local cache data directory file
+            var file = Path.Combine(FileSystem.CacheDirectory, "test.txt");
+            File.WriteAllText(file, TestStr);
+
+            // Actually get a safe shareable file uri
+            var shareableUri = Platform.GetShareableFileUri(file);
+
+            // Launch an intent to let tye user pick where to open this content
+            var intent = new Android.Content.Intent(Android.Content.Intent.ActionSend);
+            intent.SetType("text/plain");
+            intent.PutExtra(Android.Content.Intent.ExtraStream, shareableUri);
+            intent.PutExtra(Android.Content.Intent.ExtraTitle, TestStr);
+            intent.SetFlags(Android.Content.ActivityFlags.GrantReadUriPermission);
+
+            var intentChooser = Android.Content.Intent.CreateChooser(intent, TestStr);
+
+            Platform.AppContext.StartActivity(intentChooser);
+        }
+    }
+#endif
+}
