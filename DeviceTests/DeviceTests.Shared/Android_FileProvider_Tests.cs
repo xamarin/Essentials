3147070f7a7f868b91df614fbad643eab09c25c3
Redth <jondick@gmail.com>
2018-07-26 10:45:07 -0400

    Reorder using statements

--- DeviceTests/DeviceTests.Shared/Android_FileProvider_Tests.cs
+++ DeviceTests/DeviceTests.Shared/Android_FileProvider_Tests.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using Xunit;
-using Xamarin.Essentials;
 using System.IO;
+using System.Text;
+using Xamarin.Essentials;
+using Xunit;
 
 namespace DeviceTests.Shared
 {
