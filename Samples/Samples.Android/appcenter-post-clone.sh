1be13d76045c0190104ff3d78596419925dafc65
James Montemagno <james.montemagno@gmail.com>
2018-10-19 15:16:48 -0700

    cat test

--- Samples/Samples.Android/appcenter-post-clone.sh
+++ Samples/Samples.Android/appcenter-post-clone.sh
@@ -4,5 +4,5 @@ echo "Variables:"
 
 # Updating manifest
 sed -i '' "s/AC_ANDROID/$AC_ANDROID/g" $BUILD_REPOSITORY_LOCALPATH/Samples/Samples/App.xaml.cs
-
+cat $BUILD_REPOSITORY_LOCALPATH/Samples/Samples/App.xaml.cs
 echo "Manifest updated!"
