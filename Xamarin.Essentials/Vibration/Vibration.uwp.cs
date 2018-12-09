e6d06b0ca4316d79f7978f38e4d4ad2c57767882
Paul Datsiuk <jthotbox@gmail.com>
2018-12-09 14:41:45 +0200

    support of xbox gamepad vibration

--- Xamarin.Essentials/Vibration/Vibration.uwp.cs
+++ Xamarin.Essentials/Vibration/Vibration.uwp.cs
@@ -1,20 +1,55 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Windows.Foundation.Metadata;
+using Windows.Gaming.Input;
 using Windows.Phone.Devices.Notification;
 
 namespace Xamarin.Essentials
 {
     public static partial class Vibration
     {
+        static Gamepad firstGamepad;
+
+        static Gamepad FirstGamepad
+        {
+            get
+            {
+                if (firstGamepad == null)
+                    firstGamepad = Gamepad.Gamepads.FirstOrDefault();
+
+                return firstGamepad;
+            }
+        }
+
         internal static bool IsSupported
-            => ApiInformation.IsTypePresent("Windows.Phone.Devices.Notification.VibrationDevice") && DefaultDevice != null;
+            => FirstGamepad != null || (ApiInformation.IsTypePresent("Windows.Phone.Devices.Notification.VibrationDevice") && DefaultDevice != null);
 
         static VibrationDevice DefaultDevice => VibrationDevice.GetDefault();
 
-        static void PlatformVibrate(TimeSpan duration) =>
-            DefaultDevice.Vibrate(duration);
+        static void PlatformVibrate(TimeSpan duration)
+        {
+            if (FirstGamepad != null)
+            {
+                FirstGamepad.Vibration = new GamepadVibration { RightMotor = 0.85 };
+                Task.Delay(duration).ContinueWith((t) => FirstGamepad.Vibration = new GamepadVibration { });
+            }
+            else
+            {
+                DefaultDevice.Vibrate(duration);
+            }
+        }
 
-        static void PlatformCancel() =>
-            DefaultDevice.Cancel();
+        static void PlatformCancel()
+        {
+            if (FirstGamepad != null)
+            {
+                FirstGamepad.Vibration = new GamepadVibration { };
+            }
+            else
+            {
+                DefaultDevice.Cancel();
+            }
+        }
     }
 }
