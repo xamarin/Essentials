cbc249b76f138e423779c66a5fd58a1d03db7ffa
James Montemagno <james.montemagno@gmail.com>
2018-12-13 11:34:49 -0800

    Add UWP colors and adjsut android colors.

--- Xamarin.Essentials/Types/PlatformExtensions/ColorExtensions.android.cs
+++ Xamarin.Essentials/Types/PlatformExtensions/ColorExtensions.android.cs
@@ -6,7 +6,7 @@ namespace Xamarin.Forms.Platform.Android
     public static class ColorExtensions
     {
         public static AColor ToAndroidColor(this Color color) =>
-            new AColor((byte)(byte.MaxValue * color.R), (byte)(byte.MaxValue * color.G), (byte)(byte.MaxValue * color.B), (byte)(byte.MaxValue * color.A));
+            new AColor(color.R, color.G, color.B, color.A);
 
         public static Color ToColor(this AColor color) =>
             Color.FromArgb(color.A, color.R, color.G, color.B);
---
+++ Xamarin.Essentials/Types/PlatformExtensions/ColorExtensions.uwp.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using WindowsColor = Windows.UI.Color;
+
+namespace Xamarin.Essentials
+{
+    public static class ColorExtensions
+    {
+        public static WindowsColor ToWindowsColor(this Color color) =>
+               WindowsColor.FromArgb(color.A, color.R, color.G, color.B);
+
+        public static Color ToColor(this WindowsColor color) =>
+            Color.FromArgb(color.A, color.R, color.G, color.B);
+    }
+}
