1d06d34458fad212be36cd69b77d64122cb2c6d8
Pedro Jesus <pedro.pj.souza@hotmail.com>
2019-01-24 22:20:21 -0200

    Save contact in iOS

--- Xamarin.Essentials/Contacts/Contact.ios.cs
+++ Xamarin.Essentials/Contacts/Contact.ios.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using AddressBook;
+using Contacts;
+using ContactsUI;
+using Foundation;
 
 namespace Xamarin.Essentials
 {
@@ -12,6 +13,27 @@ namespace Xamarin.Essentials
             return null;
         }
 
-        static Task PlataformSaveContactAsync(string name, string phone, string email) => throw new NotImplementedException();
+        static Task PlataformSaveContactAsync(string name, string phone, string email)
+        {
+            var store = new CNContactStore();
+            var contact = new CNMutableContact
+            {
+                GivenName = name
+            };
+
+            var cellPhone = new CNLabeledValue<CNPhoneNumber>(CNLabelPhoneNumberKey.Mobile, new CNPhoneNumber(phone));
+            var phoneNumber = new[] { cellPhone };
+            contact.PhoneNumbers = phoneNumber;
+
+            var personalEmail = new CNLabeledValue<NSString>(CNLabelKey.Home, new NSString(email));
+            var emailC = new[] { personalEmail };
+            contact.EmailAddresses = emailC;
+
+            var parent = Platform.GetCurrentViewController(true);
+            var view = CNContactViewController.FromNewContact(contact);
+            view.Delegate = new ContactSaveDelegate();
+            parent.PresentViewController(view, true, null);
+            return Task.CompletedTask;
+        }
     }
 }
