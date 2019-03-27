27782848234b4f8f18f10be0ff9a155b04c37698
Pedro Jesus <pedro.pj.souza@hotmail.com>
2019-03-27 17:30:26 -0300

    - readonly struct

--- Xamarin.Essentials/Types/PhoneContact.shared.cs
+++ Xamarin.Essentials/Types/PhoneContact.shared.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Xamarin.Essentials
 {
-    public struct PhoneContact : IEquatable<PhoneContact>
+    public readonly struct PhoneContact : IEquatable<PhoneContact>
     {
         public string Name { get; }
 
