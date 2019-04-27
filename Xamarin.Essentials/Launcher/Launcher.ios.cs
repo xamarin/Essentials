7711406175560abdf279dc4d1f24668f44974f5a
James Montemagno <james.montemagno@gmail.com>
2019-04-27 13:30:49 -0700

    Implement Open File in Launcher.  Fixed #601 & #425

--- Xamarin.Essentials/Launcher/Launcher.ios.cs
+++ Xamarin.Essentials/Launcher/Launcher.ios.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using CoreGraphics;
 using Foundation;
 using UIKit;
 
@@ -12,5 +14,18 @@ namespace Xamarin.Essentials
 
         static Task PlatformOpenAsync(Uri uri) =>
             UIApplication.SharedApplication.OpenUrlAsync(new NSUrl(uri.OriginalString), new UIApplicationOpenUrlOptions());
+
+        static Task PlatformOpenAsync(OpenFileRequest request)
+        {
+            var fileUrl = NSUrl.FromFilename(request.File.FullPath);
+
+            var documentController = UIDocumentInteractionController.FromUrl(fileUrl);
+            documentController.Uti = request.File.ContentType;
+
+            var vc = Platform.GetCurrentViewController();
+
+            documentController.PresentOpenInMenu(vc.View.Frame, vc.View, true);
+            return Task.CompletedTask;
+        }
     }
 }
--- Xamarin.Essentials/Launcher/Launcher.shared.cs
+++ Xamarin.Essentials/Launcher/Launcher.shared.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Xamarin.Essentials
@@ -35,6 +36,78 @@ namespace Xamarin.Essentials
                 throw new ArgumentNullException(nameof(uri));
 
             return PlatformOpenAsync(uri);
+        }
+
+        public static Task OpenAsync(OpenFileRequest request)
+        {
+            ExperimentalFeatures.VerifyEnabled(ExperimentalFeatures.ShareFileRequest);
+
+            return PlatformOpenAsync(request);
+        }
+    }
+
+    public class OpenFileRequest
+    {
+        public OpenFileRequest()
+        {
+            ExperimentalFeatures.VerifyEnabled(ExperimentalFeatures.OpenFileRequest);
+        }
+
+        public OpenFileRequest(LauncherFile file)
+        {
+            ExperimentalFeatures.VerifyEnabled(ExperimentalFeatures.OpenFileRequest);
+            File = file;
+        }
+
+        public OpenFileRequest(FileBase file)
+        {
+            ExperimentalFeatures.VerifyEnabled(ExperimentalFeatures.OpenFileRequest);
+            File = new LauncherFile(file);
+        }
+
+        public LauncherFile File { get; set; }
+    }
+
+    public class LauncherFile : FileBase
+    {
+        public LauncherFile(string fullPath)
+            : base(fullPath)
+        {
+            ExperimentalFeatures.VerifyEnabled(ExperimentalFeatures.OpenFileRequest);
+        }
+
+        public LauncherFile(string fullPath, string contentType)
+            : base(fullPath, contentType)
+        {
+            ExperimentalFeatures.VerifyEnabled(ExperimentalFeatures.OpenFileRequest);
+        }
+
+        public LauncherFile(FileBase file)
+            : base(file)
+        {
+            ExperimentalFeatures.VerifyEnabled(ExperimentalFeatures.OpenFileRequest);
+        }
+
+        string attachmentName;
+
+        public string AttachmentName
+        {
+            get => GetAttachmentName();
+            set => attachmentName = value;
+        }
+
+        internal string GetAttachmentName()
+        {
+            // try the provided file name
+            if (!string.IsNullOrWhiteSpace(attachmentName))
+                return attachmentName;
+
+            // try get from the path
+            if (!string.IsNullOrWhiteSpace(FullPath))
+                return Path.GetFileName(FullPath);
+
+            // this should never happen as the path is validated in the constructor
+            throw new InvalidOperationException($"Unable to determine the attachment file name from '{FullPath}'.");
         }
     }
 }
