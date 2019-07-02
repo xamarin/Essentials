067f829fcadea4c50963d52bd2100ce8c5969af6
sung-su.kim <sung-su.kim@samsung.com>
2019-07-03 03:52:44 +0900

    GH-23: Added Tizen backend (#555)
    * Adding the initial work to get Tizen started. #23
    
    * Adding the initial work to get Tizen started. #23
    
    * Use the overloads properly.
    
    * Use the overloads properly.
    
    * Tizen needs to have a background explicitly set
    
    * Added the Vibration API
    
    * Added the Vibration API
    
    * Refactoring a little bit
    
    * Refactoring a little bit
    
    * Added the Browser API and some basic permissions checking
    
    * Added the Browser API and some basic permissions checking
    
    * Added the Battery API
    
    * Added the Battery API
    
    * Added the Acceleromerter API
    
    * Added the Acceleromerter API
    
    * Added the Filesystem API
    
    * Added the Filesystem API
    
    * Update Accelerometer Initialize using GetDefaultSensor
    
    * Update Accelerometer Initialize using GetDefaultSensor
    
    * Added the Gyroscope API
    
    * Added the Gyroscope API
    
    * Added the Magnetometer API
    
    * Added the Magnetometer API
    
    * Added the Compass API
    
    * Added the Compass API
    
    * Added the Connectivity API
    
    * Added the Connectivity API
    
    * Added the Flashlight API
    
    * Added the Flashlight API
    
    * Added the SecureStorage API
    
    * Added the SecureStorage API
    
    * Added the Vibration API
    
    * Added the Vibration API
    
    * Fixed build break
    
    * Fixed build break
    
    * Added the OrientationSensor API
    
    * Fixed build break
    
    * Initialized Maps/Launcher for Tizen
    
    * Added the Permissions API
    
    * Added the Geolocation API
    
    * Added the Geocoding API
    
    * Fixed build break
    
    * Initialize Barometer API
    
    * Added the Barometer API
    
    * Added the TextToSpeech API
    
    * Added the Launcher APIs
    
    * Fixed bugs
    
    * Update Location property name
    
    * Update several modules
    
    - Update AppInfo, Browser, Clipboard, Compass, Connectivity,
      DisplayInfo, DeviceInfo, Geocoding, TextToSpeech modules
    - Change module name DataTrasfer to Share, Maps to Map
    - Integrated module Power to Battery, ScreenLock to DeviceDisplay
    
    * Throw PlatformNotSupportedException for Tizen
    
    * Change the exception for the power saver feature
    
    * Add tizen privileges
    
    * Add FileBase
    
    * Add ShareFileRequest
    
    * Change enum for Browser
    
    * Change parameter for Permissions
    
    * Add SensorSpeedExtensions
    
    * Add Launcher for OpenFileRequest
    
    * Fix .csproj for VS2019
    
    * Fix sample for watch
    
    * Fix Geolocation speed
    
    * Fix TextToSpeech ptich
    
    * Fixed missing using System;
    
    This was needed for the Math calls

---
+++ Xamarin.Essentials/DeviceDisplay/DeviceDisplay.tizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Xamarin.Essentials
+{
+    public static partial class DeviceDisplay
+    {
+        static bool PlatformKeepScreenOn
+        {
+            get => throw new PlatformNotSupportedException("This API is not currently supported on Tizen.");
+            set => throw new PlatformNotSupportedException("This API is not currently supported on Tizen.");
+        }
+
+        static DisplayInfo GetMainDisplayInfo()
+            => throw new PlatformNotSupportedException("This API is not currently supported on Tizen.");
+
+        static void StartScreenMetricsListeners()
+            => throw new PlatformNotSupportedException("This API is not currently supported on Tizen.");
+
+        static void StopScreenMetricsListeners()
+            => throw new PlatformNotSupportedException("This API is not currently supported on Tizen.");
+    }
+}
