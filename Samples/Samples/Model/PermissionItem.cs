fc9e6f53440af0deceafb2b93cc50eb000374c06
Jonathan Dick <jondick@gmail.com>
2019-10-24 11:25:09 -0400

    Create sample for permissions API

---
+++ Samples/Samples/Model/PermissionItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Samples.Model
+{
+    public class PermissionItem : INotifyPropertyChanged
+    {
+        public PermissionItem(string title, Permissions.BasePermission permission)
+        {
+            Title = title;
+            Permission = permission;
+            Status = PermissionStatus.Unknown;
+        }
+
+        public string Title { get; set; }
+
+        public PermissionStatus Status { get; set; }
+
+        public Permissions.BasePermission Permission { get; set; }
+
+        public ICommand CheckStatusCommand =>
+            new Command(async () =>
+            {
+                Status = await Permission.CheckStatusAsync();
+                NotifyPropertyChanged(nameof(Status));
+            });
+
+        public ICommand RequestCommand =>
+            new Command(async () =>
+            {
+                Status = await Permission.RequestAsync();
+                NotifyPropertyChanged(nameof(Status));
+            });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string name)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
