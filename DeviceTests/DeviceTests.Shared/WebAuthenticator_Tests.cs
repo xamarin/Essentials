ff0d972bb4249f0fc3c86b752f0fd8598bf5cd3b
Jonathan Dick <jondick@gmail.com>
2020-01-17 10:26:53 -0500

    Added some tests

---
+++ DeviceTests/DeviceTests.Shared/WebAuthenticator_Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xunit;
+
+namespace DeviceTests
+{
+    public class WebAuthenticator_Tests
+    {
+        [Theory]
+        [InlineData(
+            "https://xamarin-essentials-auth-sample.azurewebsites.net/redirect",
+            "xamarinessentials",
+            "testtokenvalue",
+            "testrefreshvalue",
+            99)]
+        public async Task Redirect(string urlBase, string callbackScheme, string accessToken, string refreshToken, int expires)
+        {
+            var r = await WebAuthenticator.AuthenticateAsync(
+                new Uri($"{urlBase}?access_token={accessToken}&refresh_token={refreshToken}&expires={expires}"),
+                new Uri($"{callbackScheme}://"));
+
+            Assert.Equal(accessToken, r?.AccessToken);
+            Assert.Equal(refreshToken, r?.RefreshToken);
+            Assert.Equal(expires, (int)(r?.ExpiresIn.TotalSeconds ?? -1));
+        }
+
+        [Theory]
+        [InlineData("xamarinessentials://#access_token=blah&refresh_token=blah2&expires=1", "blah", "blah2", "1")]
+        [InlineData("xamarinessentials://?access_token=blah&refresh_token=blah2&expires=1", "blah", "blah2", "1")]
+        public void ParseQueryString(string url, string accessToken, string refreshToken, string expires)
+        {
+            var r = WebUtils.ParseQueryString(url);
+
+            Assert.Equal(accessToken, r?["access_token"]);
+            Assert.Equal(refreshToken, r?["refresh_token"]);
+            Assert.Equal(expires, r?["expires"]);
+        }
+    }
+}
