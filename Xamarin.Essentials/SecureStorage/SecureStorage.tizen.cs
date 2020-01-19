ed7959b238be26ce59ceb79446fca41e0a3ed62c
Damian Mehers <damian@mehers.com>
2020-01-19 15:04:49 +0100

    Missing exception namespace

--- Xamarin.Essentials/SecureStorage/SecureStorage.tizen.cs
+++ Xamarin.Essentials/SecureStorage/SecureStorage.tizen.cs
@@ -14,7 +14,7 @@ namespace Xamarin.Essentials
                 // a password
                 return Task.FromResult(Encoding.UTF8.GetString(DataManager.Get(key, null)));
             }
-            catch (InvalidOperationException)
+            catch (System.InvalidOperationException)
             {
                 // The DataManager.Get call throws an exception if key does not exist. Not logging
                 // anything since this is an expected and normal situation (if the key did not exist).
