29f9d97b30d6d28293f1c9b6a79e66665d74979c
Damian Mehers <damian@mehers.com>
2020-01-19 14:53:45 +0100

    Simplified and clarified. Fix GetAsync

--- Xamarin.Essentials/SecureStorage/SecureStorage.tizen.cs
+++ Xamarin.Essentials/SecureStorage/SecureStorage.tizen.cs
@@ -10,7 +10,15 @@ namespace Xamarin.Essentials
         {
             try
             {
+                // The null parameter here is not the default value as you might expect, but
+                // a password
                 return Task.FromResult(Encoding.UTF8.GetString(DataManager.Get(key, null)));
+            }
+            catch(InvalidOperationException)
+            {
+                // The DataManager.Get call throws an exception if key does not exist. Not logging
+                // anything since this is an expected and normal situation (if the key did not exist).
+                return null; 
             }
             catch
             {
@@ -25,16 +33,14 @@ namespace Xamarin.Essentials
             {
                 try
                 {
-                    // This throws an exception if key does not exist. The second parameter is not the default value.
-                    var existing = DataManager.Get(key, null);
-                    if (existing != null)
-                    {
-                        DataManager.RemoveAlias(key);
-                    }
+                    // Remove the key in case it already exists, otherwise DataManager.Save will throw an exception.
+                    // There is no way to check if a key exists without throwing an exception, so there is
+                    // no point in checking whether the key exists prior to attempting to remove it.
+                    DataManager.RemoveAlias(key);
                 }
                 catch
                 {
-                    Tizen.Log.Error(Platform.CurrentPackage.Label, "Key did not exist when setting");
+                    // Not logging anything since this is an expected and normal situation (if the key did not exist).
                 }
 
                 DataManager.Save(key, Encoding.UTF8.GetBytes(data), new Policy());
