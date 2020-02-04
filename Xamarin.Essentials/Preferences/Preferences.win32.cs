eed2900c2e3d5d7ebd3d48a011d54653b752b7f1
Peter Freeman Foot <peter@inthehand.com>
2020-02-04 10:13:32 +0000

    Preferences persisted to file. Email provider added

--- Xamarin.Essentials/Preferences/Preferences.win32.cs
+++ Xamarin.Essentials/Preferences/Preferences.win32.cs
@@ -1,40 +1,113 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Xamarin.Essentials
 {
     public static partial class Preferences
     {
-        static Dictionary<string, object> preferences = new Dictionary<string, object>();
+        static readonly string settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppInfo.Name, "settings.dat");
 
-        static bool PlatformContainsKey(string key, string sharedName) =>
-            preferences.ContainsKey(key);
+        static Dictionary<string, Dictionary<string, string>> preferences = new Dictionary<string, Dictionary<string, string>>();
 
-        static void PlatformRemove(string key, string sharedName) =>
-            preferences.Remove(key);
-
-        static void PlatformClear(string sharedName) =>
-            preferences.Clear();
-
-        static void PlatformSet<T>(string key, T value, string sharedName)
+        static Preferences()
         {
-            if (preferences.ContainsKey(key))
+            if (File.Exists(settingsPath))
             {
-                preferences[key] = value;
+                var json = File.ReadAllText(settingsPath);
+                try
+                {
+                    preferences = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(json);
+                }
+                catch
+                {
+                    File.Delete(settingsPath);
+                }
             }
             else
             {
-                preferences.Add(key, value);
+                preferences.Add(string.Empty, new Dictionary<string, string>());
+
+                // create folder for app settings
+                Directory.CreateDirectory(Path.GetDirectoryName(settingsPath));
             }
+        }
+
+        static void Save()
+        {
+            var json = JsonConvert.SerializeObject(preferences);
+            File.WriteAllText(settingsPath, json);
+        }
+
+        static string CleanSharedName(string sharedName)
+        {
+            if (string.IsNullOrEmpty(sharedName))
+            {
+                return string.Empty;
+            }
+
+            return sharedName;
+        }
+
+        static bool PlatformContainsKey(string key, string sharedName)
+        {
+            if (preferences.ContainsKey(CleanSharedName(sharedName)))
+            {
+                return preferences[sharedName].ContainsKey(key);
+            }
+
+            return false;
+        }
+
+        static void PlatformRemove(string key, string sharedName)
+        {
+            if (preferences.ContainsKey(CleanSharedName(sharedName)))
+            {
+                preferences[CleanSharedName(sharedName)].Remove(key);
+                Save();
+            }
+        }
+
+        static void PlatformClear(string sharedName)
+        {
+            if (preferences.ContainsKey(CleanSharedName(sharedName)))
+            {
+                preferences[CleanSharedName(sharedName)].Clear();
+                Save();
+            }
+        }
+
+        static void PlatformSet<T>(string key, T value, string sharedName)
+        {
+            if (!preferences.ContainsKey(CleanSharedName(sharedName)))
+            {
+                preferences.Add(CleanSharedName(sharedName), new Dictionary<string, string>());
+            }
+
+            if (preferences[CleanSharedName(sharedName)].ContainsKey(key))
+            {
+                preferences[CleanSharedName(sharedName)][key] = JsonConvert.SerializeObject(value);
+            }
+            else
+            {
+                preferences[CleanSharedName(sharedName)].Add(key, JsonConvert.SerializeObject(value));
+            }
+
+            Save();
         }
 
         static T PlatformGet<T>(string key, T defaultValue, string sharedName)
         {
-            if (preferences.ContainsKey(key))
+            if (preferences.ContainsKey(CleanSharedName(sharedName)))
             {
-                return (T)preferences[key];
+                if (preferences[CleanSharedName(sharedName)].ContainsKey(key))
+                {
+                    return JsonConvert.DeserializeObject<T>(preferences[CleanSharedName(sharedName)][key]);
+                }
             }
 
-            return default;
+            return defaultValue;
         }
     }
 }
