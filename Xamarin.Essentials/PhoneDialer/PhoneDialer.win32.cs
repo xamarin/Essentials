41f2cad119d5a0f746826f3fb69b8cc53dc7d39b
Peter Freeman Foot <peter@inthehand.com>
2020-02-29 15:33:13 +0000

    Added phonedialer support for completeness

---
+++ Xamarin.Essentials/PhoneDialer/PhoneDialer.win32.cs
@@ -0,0 +1,14 @@
+﻿namespace Xamarin.Essentials
+{
+    public static partial class PhoneDialer
+    {
+        internal static bool IsSupported => true;
+
+        static void PlatformOpen(string number)
+        {
+            ValidateOpen(number);
+
+            Launcher.OpenAsync("tel:" + number);
+        }
+    }
+}
