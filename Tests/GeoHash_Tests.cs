7cc5de7464bce043ba62e56303cdcb88831b8417
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-27 22:51:42 +0530

    bug fixes

--- Tests/GeoHash_Tests.cs
+++ Tests/GeoHash_Tests.cs
@@ -15,7 +15,7 @@ namespace Tests
         public void EncodeFromCoords()
         {
             var actual8 = "eusftqx9";
-            string calculated8 = GeoHash.Encode(25.78792, -4.32913, 8);
+            var calculated8 = GeoHash.Encode(25.78792, -4.32913, 8);
             Assert.Equal(actual8, calculated8);
             Assert.Equal("efkbt6rx", GeoHash.Encode(new GeoPoint(12.7578, -4.32913), 8));
         }
@@ -32,7 +32,6 @@ namespace Tests
         [Fact]
         public void DirectionEncodeTest()
         {
-
             Assert.Equal("eusftqxd", GeoHash.Adjacent("eusftqx9", Direction.North));
             Assert.Equal("eusftqx8", GeoHash.Adjacent("eusftqx9", Direction.South));
             Assert.Equal("eusftqxc", GeoHash.Adjacent("eusftqx9", Direction.East));
