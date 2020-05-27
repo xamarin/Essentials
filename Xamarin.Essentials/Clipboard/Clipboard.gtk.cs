6a330929e2e3a61e55db36f8fd0e0a6871e69ae5
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-05-27 11:43:05 +0200

    Clipboard: add GTK support
    Based on https://github.com/stavroskasidis/XamarinClipboardPlugin/commit/b213a88d3d9d85d3600cd5084a53c670f51f0fb7
    
    Remove non-required compiler file includes

---
+++ Xamarin.Essentials/Clipboard/Clipboard.gtk.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+
+namespace Xamarin.Essentials
+{
+    public static partial class Clipboard
+    {
+#if GTK
+        private static readonly Gdk.Atom clipboardAtom = Gdk.Atom.Intern("CLIPBOARD", false);
+
+        static Task PlatformSetTextAsync(string text)
+        {
+            var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+            clipboard.Text = text;
+            return Task.FromResult(0);
+        }
+
+        static bool PlatformHasText
+        {
+            get
+            {
+                var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+                return clipboard.WaitIsTextAvailable();
+            }
+        }
+
+        static Task<string> PlatformGetTextAsync()
+        {
+            var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+            return Task.FromResult(clipboard.WaitForText());
+        }
+#else
+        static Task PlatformSetTextAsync(string text)
+            => throw ExceptionUtils.NotSupportedOrImplementedException;
+
+        static bool PlatformHasText
+            => throw ExceptionUtils.NotSupportedOrImplementedException;
+
+        static Task<string> PlatformGetTextAsync()
+            => throw ExceptionUtils.NotSupportedOrImplementedException;
+
+        static void StartClipboardListeners()
+            => throw ExceptionUtils.NotSupportedOrImplementedException;
+
+        static void StopClipboardListeners()
+            => throw ExceptionUtils.NotSupportedOrImplementedException;
+#endif
+    }
+}
--- Xamarin.Essentials/MainThread/MainThread.netstandard.cs
+++ Xamarin.Essentials/MainThread/MainThread.netstandard.cs
@@ -1,13 +1,40 @@
 ﻿using System;
+#if GTK
+using System.Threading;
+#endif
 
 namespace Xamarin.Essentials
 {
     public static partial class MainThread
     {
+#if GTK
+#pragma warning disable SA1130
+        static void PlatformBeginInvokeOnMainThread(Action action) => Gtk.Application.Invoke(delegate { action(); });
+
+        static int? uiThreadId = null;
+
+        static bool PlatformIsMainThread
+        {
+            get
+            {
+                if (uiThreadId == null)
+                {
+                    Gtk.Application.Invoke(delegate { uiThreadId = Thread.CurrentThread.ManagedThreadId; });
+                    return false;
+                }
+                else
+                {
+                    return System.Threading.Thread.CurrentThread.ManagedThreadId == uiThreadId;
+                }
+            }
+        }
+#pragma warning restore SA1130
+#else
         static void PlatformBeginInvokeOnMainThread(Action action) =>
             throw ExceptionUtils.NotSupportedOrImplementedException;
 
         static bool PlatformIsMainThread =>
             throw ExceptionUtils.NotSupportedOrImplementedException;
+#endif
     }
 }
