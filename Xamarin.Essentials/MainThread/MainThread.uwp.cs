c1d028e9ad532c999db7770f6c602d01cac3b812
Schilli, Niklas <schilli@ams-berlin.de>
2020-10-06 08:41:51 +0200

    Reverted more exception to correct type

--- Xamarin.Essentials/MainThread/MainThread.uwp.cs
+++ Xamarin.Essentials/MainThread/MainThread.uwp.cs
@@ -34,7 +34,7 @@ namespace Xamarin.Essentials
             var dispatcher = CoreApplication.MainView?.CoreWindow?.Dispatcher;
 
             if (dispatcher == null)
-                ThrowHelper.ThrowArgumentNullException("Unable to find main thread.");
+                ThrowHelper.ThrowInvalidOperationException("Unable to find main thread.");
             dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action()).WatchForError();
         }
     }
