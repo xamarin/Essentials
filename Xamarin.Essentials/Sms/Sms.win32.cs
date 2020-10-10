01faa599bf2c8a924f0370c4dc02aa5e405c6465
Peter Foot <peter@inthehand.com>
2020-10-10 14:07:50 +0100

    Added a basic Sms provider for Win32 using an sms: uri which will launch an associated app if available

---
+++ Xamarin.Essentials/Sms/Sms.win32.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xamarin.Essentials
+{
+    public static partial class Sms
+    {
+        internal static bool IsComposeSupported
+            => true;
+
+        static Task PlatformComposeAsync(SmsMessage message)
+        {
+            return Launcher.OpenAsync("sms:" + CombineRecipients(message.Recipients) + (string.IsNullOrEmpty(message.Body) ? string.Empty : "?body=" + Uri.EscapeUriString(message.Body)));
+        }
+
+        static string CombineRecipients(System.Collections.Generic.List<string> recipients)
+        {
+            if (recipients == null)
+                return string.Empty;
+
+            var stringBuilder = new System.Text.StringBuilder();
+
+            foreach (var recipient in recipients)
+            {
+                stringBuilder.Append(recipient + ",");
+            }
+
+            if (stringBuilder.Length > 0)
+                stringBuilder.Length -= 1;
+
+            return stringBuilder.ToString();
+        }
+    }
+}
