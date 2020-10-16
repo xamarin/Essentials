5e61f67d6bdb4957d47acac5d354b8a2601a03ef
sung-su.kim <sung-su.kim@samsung.com>
2020-10-16 09:18:41 +0900

    Add type extensions for Tizen

---
+++ Xamarin.Essentials/Types/PlatformExtensions/RectangleExtensions.tizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using ERect = ElmSharp.Rect;
+
+namespace Xamarin.Essentials
+{
+    public static class RectangleExtensions
+    {
+        public static Rectangle ToSystemRectangle(this ERect rect) =>
+            new Rectangle(rect.Left, rect.Top, rect.Width, rect.Height);
+
+        public static RectangleF ToSystemRectangleF(this ERect rect) =>
+            new RectangleF(rect.Left, rect.Top, rect.Width, rect.Height);
+
+        public static ERect ToPlatformRectangle(this Rectangle rect) =>
+            new ERect(rect.Left, rect.Top, rect.Right, rect.Bottom);
+
+        public static ERect ToPlatformRectangleF(this RectangleF rect) =>
+            new ERect((int)rect.Left, (int)rect.Top, (int)rect.Right, (int)rect.Bottom);
+    }
+}
