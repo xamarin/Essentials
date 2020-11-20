9c6e421ef745de52977cb2d2f036805db7be2e1b
Matthew Leibowitz <mattleibow@live.com>
2020-11-20 19:16:04 +0200

    Make everything writeable

--- Xamarin.Essentials/Types/Contact.shared.cs
+++ Xamarin.Essentials/Types/Contact.shared.cs
@@ -5,43 +5,55 @@ namespace Xamarin.Essentials
 {
     public class Contact
     {
-        internal Contact(string name, IEnumerable<ContactPhone> phones, IEnumerable<ContactEmail> email)
+        public Contact()
+        {
+        }
+
+        public Contact(string name, IEnumerable<ContactPhone> phones, IEnumerable<ContactEmail> email)
         {
             Name = name;
             Emails = email?.ToList();
             Phones = phones?.ToList();
         }
 
-        public string Name { get; }
+        public string Name { get; set; }
 
-        public List<ContactPhone> Phones { get; }
+        public List<ContactPhone> Phones { get; set; } = new List<ContactPhone>();
 
-        public List<ContactEmail> Emails { get; }
+        public List<ContactEmail> Emails { get; set; } = new List<ContactEmail>();
     }
 
     public class ContactEmail
     {
-        internal ContactEmail(string value, ContactType type)
+        public ContactEmail()
+        {
+        }
+
+        public ContactEmail(string value, ContactType type)
         {
             Value = value;
             Type = type;
         }
 
-        public string Value { get; }
+        public string Value { get; set; }
 
-        public ContactType Type { get; }
+        public ContactType Type { get; set; }
     }
 
     public class ContactPhone
     {
-        internal ContactPhone(string value, ContactType type)
+        public ContactPhone()
+        {
+        }
+
+        public ContactPhone(string value, ContactType type)
         {
             Value = value;
             Type = type;
         }
 
-        public string Value { get; }
+        public string Value { get; set; }
 
-        public ContactType Type { get; }
+        public ContactType Type { get; set; }
     }
 }
