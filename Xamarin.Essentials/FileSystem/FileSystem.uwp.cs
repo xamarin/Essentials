4869b3025b522de97dbf23ec1530a8bf167958c8
Dominique Louis <savagesoftware@gmail.com>
2020-12-04 14:39:16 +0000

    [UWP] Package and DisplayName missing. Fixed.

--- Xamarin.Essentials/FileSystem/FileSystem.uwp.cs
+++ Xamarin.Essentials/FileSystem/FileSystem.uwp.cs
@@ -26,10 +26,10 @@ namespace Xamarin.Essentials
             => path.Replace('/', Path.DirectorySeparatorChar);
 
         static string[] PlatformGetAppResourceDirectories(string path)
-            => Directory.GetDirectories(Path.Combine(Application.Current.InstalledLocation, NormalizePath(path)));
+            => Directory.GetDirectories(Path.Combine(Package.Current.InstalledLocation.DisplayName, NormalizePath(path)));
 
         static string[] PlatformGetAppResourceFiles(string path)
-            => Directory.GetFiles(Path.Combine(Application.Current.InstalledLocation, NormalizePath(path)));
+            => Directory.GetFiles(Path.Combine(Package.Current.InstalledLocation.DisplayName, NormalizePath(path)));
     }
 
     public partial class FileBase
