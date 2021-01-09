819c6f9b636f16d21939aa5eaf8bd600d800b803
dimonovdd <dimovde@yandex.ru>
2021-01-10 00:37:09 +0300

    init SaveToGalery

---
+++ Samples/Samples/ViewModel/SaveToGaleryViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Essentials.SaveToGalery;
+using Xamarin.Forms;
+
+namespace Samples.ViewModel
+{
+    public class SaveToGaleryViewModel : BaseViewModel
+    {
+        public SaveToGaleryViewModel()
+            => SaveCommand = new Command(Save);
+
+        public ICommand SaveCommand { get; }
+
+        public string ImageUrl
+            => "https://raw.githubusercontent.com/xamarin/Essentials/main/Assets/xamarin.essentials_128x128.png";
+
+        async void Save()
+        {
+            try
+            {
+                using var client = new WebClient();
+                var data = await client.DownloadDataTaskAsync(ImageUrl);
+                await SaveToGalery.SaveImageAsync(data, "hhhh.png");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync(ex.Message);
+            }
+        }
+    }
+}
---
+++ Xamarin.Essentials/SaveToGalery/SaveToGalery.shared.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Xamarin.Essentials.SaveToGalery
+{
+    public static partial class SaveToGalery
+    {
+        public static Task SaveImageAsync(byte[] data, string filename, string albumName = null)
+            => PlatformSaveImageAsync(data, filename, albumName);
+    }
+}
