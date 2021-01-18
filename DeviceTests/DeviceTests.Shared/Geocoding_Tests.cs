74a6ab6b100b1fa38109ecf5cfbbd9260a099e9c
Matthew Leibowitz <mattleibow@live.com>
2021-01-18 23:39:54 +0200

    Update Geocoding_Tests.cs

--- DeviceTests/DeviceTests.Shared/Geocoding_Tests.cs
+++ DeviceTests/DeviceTests.Shared/Geocoding_Tests.cs
@@ -48,7 +48,7 @@ namespace DeviceTests
         }
 
         [Theory]
-        [InlineData("Microsoft Building 25 Redmond WA")]
+        [InlineData("Redmond, WA, USA")]
         public async Task Get_Locations(string address)
         {
             try
