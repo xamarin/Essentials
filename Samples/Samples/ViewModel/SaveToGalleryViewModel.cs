ebde333d57175b94eb144c9bc28fdfbd100a73e5
dimonovdd <dimovde@yandex.ru>
2021-01-25 01:27:21 +0300

    implementation for creating photos albums on ios

--- Samples/Samples/ViewModel/SaveToGalleryViewModel.cs
+++ Samples/Samples/ViewModel/SaveToGalleryViewModel.cs
@@ -10,19 +10,22 @@ namespace Samples.ViewModel
     public class SaveToGalleryViewModel : BaseViewModel
     {
         public SaveToGalleryViewModel()
-            => SaveCommand = new Command(Save);
+            => SaveCommand = new Command<string>(Save);
 
         public ICommand SaveCommand { get; }
 
-        public string ImageUrl
+        public string PngUrl
             => "https://raw.githubusercontent.com/xamarin/Essentials/main/Assets/xamarin.essentials_128x128.png";
 
-        async void Save()
+        public string JpgUrl
+            => "https://raw.githubusercontent.com/dimonovdd/Essentials/featureSaveToGalery/Assets/SaveToGaleryTestPhoto.jpg";
+
+        async void Save(string type)
         {
             try
             {
                 using var client = new WebClient();
-                var data = await client.DownloadDataTaskAsync(ImageUrl);
+                var data = await client.DownloadDataTaskAsync(type == "png" ? PngUrl : JpgUrl);
                 await SaveToGallery.SaveImageAsync(data, "hhhh.png");
             }
             catch (Exception ex)
--- Xamarin.Essentials/SaveToGallery/SaveToGallery.ios.cs
+++ Xamarin.Essentials/SaveToGallery/SaveToGallery.ios.cs
@@ -1,17 +1,62 @@
 ﻿using System;
 using System.Threading.Tasks;
+using CoreGraphics;
+using CoreImage;
 using Foundation;
+using ImageIO;
+using Photos;
 using UIKit;
 
 namespace Xamarin.Essentials.SaveToGallery
 {
     public static partial class SaveToGallery
     {
-        static Task PlatformSaveImageAsync(byte[] data, string filename, string albumName)
+        static async Task PlatformSaveImageAsync(byte[] data, string filename, string albumName)
         {
-            var image = new UIImage(NSData.FromArray(data));
-            image.SaveToPhotosAlbum((UIImage img, NSError error) => { });
-            return Task.CompletedTask;
+            albumName = "testAlbumName";
+            await Permissions.EnsureGrantedAsync<Permissions.Photos>();
+
+            var album = GetAlbum(albumName);
+
+            if (album == null)
+                album = await CreateAlbumAsync(albumName);
+        }
+
+        static PHAssetCollection GetAlbum(string albumName)
+        {
+            var options = new PHFetchOptions();
+            options.Predicate = NSPredicate.FromFormat(
+                    "title=%@",
+                    new NSObject[] { NSObject.FromObject(albumName) });
+
+            var collection = PHAssetCollection.FetchAssetCollections(
+                PHAssetCollectionType.Album,
+                PHAssetCollectionSubtype.Any,
+                options);
+
+            return (PHAssetCollection)collection?.firstObject;
+        }
+
+        static async Task<PHAssetCollection> CreateAlbumAsync(string albumName)
+        {
+            PHObjectPlaceholder placeholder = null;
+
+            var tcs = new TaskCompletionSource<NSError>();
+            PHPhotoLibrary.SharedPhotoLibrary.PerformChanges(
+                () =>
+                {
+                    var request = PHAssetCollectionChangeRequest.CreateAssetCollection(albumName);
+                    placeholder = request.PlaceholderForCreatedAssetCollection;
+                },
+                (success, error) => tcs.TrySetResult(error));
+
+            var error = await tcs.Task;
+            if (error != null)
+                throw new NSErrorException(error);
+
+            var id = placeholder?.LocalIdentifier;
+            var fetchResult = PHAssetCollection.FetchAssetCollections(new string[] { id }, null);
+            return (PHAssetCollection)fetchResult.firstObject;
         }
     }
 }
