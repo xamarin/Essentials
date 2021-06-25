d40ff5678851fea8737543b8313d5a1409fdb018
Matthew Leibowitz <mattleibow@live.com>
2021-06-25 19:11:06 +0200

    Update AssemblyInfo.ios.tvos.watchos.macos.cs

--- Xamarin.Essentials/AssemblyInfo/AssemblyInfo.ios.tvos.watchos.macos.cs
+++ Xamarin.Essentials/AssemblyInfo/AssemblyInfo.ios.tvos.watchos.macos.cs
@@ -1,7 +1,5 @@
 ﻿using Foundation;
 
-[assembly: System.Reflection.AssemblyMetadata("IsTrimmable", "True")]
-
 #pragma warning disable CS0618 // Type or member is obsolete
 [assembly: LinkerSafe]
 #pragma warning restore CS0618 // Type or member is obsolete
