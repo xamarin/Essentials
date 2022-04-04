731988eb0caf808b6f2cfdb00db2d44f484872fd
Gerald Versluis <gerald@verslu.is>
2022-04-04 16:45:45 +0200

    Update PhoneDialer.ios.cs

--- Xamarin.Essentials/PhoneDialer/PhoneDialer.ios.cs
+++ Xamarin.Essentials/PhoneDialer/PhoneDialer.ios.cs
@@ -11,12 +11,12 @@ namespace Xamarin.Essentials
 
         internal static bool IsSupported => UIApplication.SharedApplication.CanOpenUrl(CreateNsUrl(new string('0', 10)));
 
-        static async void PlatformOpen(string number)
+        static void PlatformOpen(string number)
         {
             ValidateOpen(number);
 
             var nsUrl = CreateNsUrl(number);
-            await Launcher.PlatformOpenAsync(nsUrl);
+            Launcher.PlatformOpenAsync(nsUrl);
         }
 
         static NSUrl CreateNsUrl(string number) => new NSUrl(new Uri($"tel:{number}").AbsoluteUri);
